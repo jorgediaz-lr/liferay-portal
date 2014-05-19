b75582f6335fb19226a64ff204e8581d27e7f451
Jorge Díaz <jorge.diaz@liferay.com>
2014-05-19 18:25:59 +0200

    LPS-XXXXX Permission checking is wrong

--- portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/journal/service/impl/JournalArticleServiceImpl.java
@@ -1240,10 +1240,9 @@ public class JournalArticleServiceImpl extends JournalArticleServiceBaseImpl {
 		for (JournalArticle article : articles) {
 			JournalArticlePermission.check(
 				getPermissionChecker(), article, ActionKeys.UPDATE);
-
-			journalArticleLocalService.moveArticle(
-				groupId, articleId, newFolderId);
 		}
+
+		journalArticleLocalService.moveArticle(groupId, articleId, newFolderId);
 	}
 
 	/**
