7170d9a3dae4aaef3526a12cdde80051db3747bb
Jorge Díaz <jorge.diaz@liferay.com>
2015-06-01 16:20:59 +0200

    LPS-55055 Apply changes

--- portal-impl/src/com/liferay/portal/lar/PortletExporter.java
+++ portal-impl/src/com/liferay/portal/lar/PortletExporter.java
@@ -57,7 +57,6 @@ import com.liferay.portal.kernel.xml.Element;
 import com.liferay.portal.kernel.xml.Node;
 import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.kernel.zip.ZipWriter;
-import com.liferay.portal.kernel.zip.ZipWriterFactoryUtil;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.model.Layout;
 import com.liferay.portal.model.LayoutConstants;
@@ -1142,7 +1141,8 @@ public class PortletExporter {
 
 		Layout layout = LayoutLocalServiceUtil.getLayout(plid);
 
-		ZipWriter zipWriter = ZipWriterFactoryUtil.getZipWriter();
+		ZipWriter zipWriter = ExportImportHelperUtil.getPortletZipWriter(
+			portletId);
 
 		PortletDataContext portletDataContext =
 			PortletDataContextFactoryUtil.createExportPortletDataContext(
