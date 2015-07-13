4af521646899b5295c7fac7794f70c062cf2c8c1
Jorge Díaz <jorge.diaz@liferay.com>
2015-07-13 14:15:56 +0200

    LPS-57021 Add empty array to force AssetLink deletion

--- modules/apps/export-import/export-import-service/src/com/liferay/exportimport/lar/PortletDataContextImpl.java
+++ modules/apps/export-import/export-import-service/src/com/liferay/exportimport/lar/PortletDataContextImpl.java
@@ -2047,6 +2047,8 @@ public class PortletDataContextImpl implements PortletDataContext {
 			String[] assetTagNames = getAssetTagNames(clazz, classPK);
 
 			serviceContext.setAssetTagNames(assetTagNames);
+
+			serviceContext.setAssetLinkEntryIds(new long[0]);
 		}
 
 		// Expando
