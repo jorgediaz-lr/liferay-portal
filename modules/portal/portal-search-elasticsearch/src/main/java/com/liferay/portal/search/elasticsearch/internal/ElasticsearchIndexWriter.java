8a3db0ee77472202fb7f9b69d82c3ada64827b05
Jorge Díaz <jorge.diaz@liferay.com>
2016-01-27 14:20:50 +0100

    LPS-62665 Add "liferay-" prefix to index name

--- modules/portal/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/ElasticsearchIndexWriter.java
+++ modules/portal/portal-search-elasticsearch/src/main/java/com/liferay/portal/search/elasticsearch/internal/ElasticsearchIndexWriter.java
@@ -87,7 +87,7 @@ public class ElasticsearchIndexWriter extends BaseIndexWriter {
 
 			RefreshRequestBuilder refreshRequestBuilder =
 				indicesAdminClient.prepareRefresh(
-					String.valueOf(searchContext.getCompanyId()));
+					"liferay-"  + String.valueOf(searchContext.getCompanyId()));
 
 			RefreshResponse refreshResponse = refreshRequestBuilder.get();
 
@@ -106,7 +106,7 @@ public class ElasticsearchIndexWriter extends BaseIndexWriter {
 			Client client = _elasticsearchConnectionManager.getClient();
 
 			DeleteRequestBuilder deleteRequestBuilder = client.prepareDelete(
-				String.valueOf(searchContext.getCompanyId()),
+				"liferay-" + String.valueOf(searchContext.getCompanyId()),
 				DocumentTypes.LIFERAY, uid);
 
 			if (PortalRunMode.isTestMode() ||
@@ -137,7 +137,7 @@ public class ElasticsearchIndexWriter extends BaseIndexWriter {
 			for (String uid : uids) {
 				DeleteRequestBuilder deleteRequestBuilder =
 					client.prepareDelete(
-						String.valueOf(searchContext.getCompanyId()),
+						"liferay-" + String.valueOf(searchContext.getCompanyId()),
 						DocumentTypes.LIFERAY, uid);
 
 				bulkRequestBuilder.add(deleteRequestBuilder);
