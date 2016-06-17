382ae4dd48f66c32c63f8d0c39b5028b7e546f96
Jorge Díaz <jorge.diaz@liferay.com>
2016-06-17 11:00:23 +0200

    LPS-66570 Change isActive usages to isActiveGroup

--- portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/GroupLocalServiceImpl.java
@@ -3579,7 +3579,7 @@ public class GroupLocalServiceImpl extends GroupLocalServiceBaseImpl {
 			// Filter by active
 
 			if (active != null) {
-				if (active != group.isActive()) {
+				if (active != group.isActiveGroup()) {
 					iterator.remove();
 
 					continue;
