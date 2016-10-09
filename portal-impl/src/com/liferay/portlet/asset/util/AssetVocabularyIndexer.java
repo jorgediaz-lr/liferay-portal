1426827a609e2111029df7a733d1b0cb2fc992ca
Jorge Díaz <jorge.diaz@liferay.com>
2016-10-09 15:24:39 +0200

    LPS-57184 Call getPersistedModel

--- portal-impl/src/com/liferay/portlet/asset/util/AssetVocabularyIndexer.java
+++ portal-impl/src/com/liferay/portlet/asset/util/AssetVocabularyIndexer.java
@@ -71,7 +71,7 @@ public class AssetVocabularyIndexer extends BaseIndexer<AssetVocabulary> {
 		throws Exception {
 
 		AssetVocabulary vocabulary =
-			AssetVocabularyLocalServiceUtil.getVocabulary(entryClassPK);
+			getPersistedModel(entryClassName, entryClassPK);
 
 		return AssetVocabularyPermission.contains(
 			permissionChecker, vocabulary, ActionKeys.VIEW);
