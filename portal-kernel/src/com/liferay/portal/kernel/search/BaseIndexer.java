b4387351f752a98f65361f1acb4d7ea022c29e05
Jorge Díaz <jorge.diaz@liferay.com>
2016-10-09 15:02:00 +0200

    LPS-57184 New getPersistedModel method

--- portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/BaseIndexer.java
@@ -1438,11 +1438,8 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 	}
 
 	protected void doReindex(String className, long classPK) throws Exception {
-		PersistedModelLocalService service =
-			PersistedModelLocalServiceRegistryUtil.
-				getPersistedModelLocalService(className);
 
-		T object = (T)service.getPersistedModel(classPK);
+		T object = getPersistedModel(className, classPK);
 
 		doReindex(object);
 	}
@@ -1661,6 +1658,16 @@ public abstract class BaseIndexer<T> implements Indexer<T> {
 		}
 
 		return countryNames;
+	}
+
+	protected T getPersistedModel(String className, long classPK)
+		throws PortalException {
+
+		PersistedModelLocalService service =
+				PersistedModelLocalServiceRegistryUtil.
+					getPersistedModelLocalService(className);
+
+		return (T)service.getPersistedModel(classPK);
 	}
 
 	/**
