1d572c4729b4107450d9d8866bfe3dcd97e28288
Jorge Díaz <jorge.diaz@liferay.com>
2016-11-17 15:10:43 +0100

    LPS-68591 Calculate stringBundler size instead using '20'

--- portal-impl/src/com/liferay/portal/service/persistence/impl/UserFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/UserFinderImpl.java
@@ -478,7 +478,9 @@ public class UserFinderImpl extends UserFinderBaseImpl implements UserFinder {
 				sql = StringUtil.replace(sql, _STATUS_SQL, StringPool.BLANK);
 			}
 
-			StringBundler sb = new StringBundler(20);
+			int stringBundlerSize = (paramsMapList.size() * 4) - 1;
+
+			StringBundler sb = new StringBundler(stringBundlerSize);
 
 			for (int i = 0; i < paramsMapList.size(); i++) {
 				if (i != 0) {
@@ -785,7 +787,9 @@ public class UserFinderImpl extends UserFinderBaseImpl implements UserFinder {
 				sql = StringUtil.replace(sql, _STATUS_SQL, StringPool.BLANK);
 			}
 
-			StringBundler sb = new StringBundler(20);
+			int stringBundlerSize = (paramsMapList.size() * 4) + 1;
+
+			StringBundler sb = new StringBundler(stringBundlerSize);
 
 			for (int i = 0; i < paramsMapList.size(); i++) {
 				if (i != 0) {
