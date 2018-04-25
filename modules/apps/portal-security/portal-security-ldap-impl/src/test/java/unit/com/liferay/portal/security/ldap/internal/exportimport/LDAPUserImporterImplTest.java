8e4be4d487ded1c6604b0e8b97a8ab60a89287b1
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2018-04-25 17:25:03 +0200

    LPS-79425 Test for this bug

--- modules/apps/portal-security/portal-security-ldap-impl/src/test/java/unit/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImplTest.java
+++ modules/apps/portal-security/portal-security-ldap-impl/src/test/java/unit/com/liferay/portal/security/ldap/internal/exportimport/LDAPUserImporterImplTest.java
@@ -25,6 +25,20 @@ import org.junit.Test;
 public class LDAPUserImporterImplTest {
 
 	@Test
+	public void testBindingInNamespaceEscape() throws InvalidNameException {
+		Assert.assertEquals(
+			"cn=User\\\\,with\\\\,commas,ou=users,dc=example,dc=com",
+			escapeValue(
+				normalizeLdapName(
+					"cn=User\\,with\\,commas,ou=users,dc=example,dc=com")));
+		Assert.assertEquals(
+			"cn=User\\\\,with\\\\,commas,ou=users,dc=example,dc=com",
+			escapeValue(
+				normalizeLdapName(
+					"cn=User\\2cwith\\2ccommas,ou=users,dc=example,dc=com")));
+	}
+
+	@Test
 	public void testCleanupLdapName() throws InvalidNameException {
 		Assert.assertEquals(
 			"cn=test test,ou=test",
