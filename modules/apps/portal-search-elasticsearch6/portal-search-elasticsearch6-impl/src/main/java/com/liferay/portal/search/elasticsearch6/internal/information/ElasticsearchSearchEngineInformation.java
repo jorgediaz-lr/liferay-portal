ba3137de135137ff2fc3f4337abe599a39fadced
Jorge Díaz <jorge.diaz@liferay.com>
2019-02-20 20:24:24 +0100

    LPS-87931 Display cluster version and nodes in case operation mode=remote

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/information/ElasticsearchSearchEngineInformation.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/information/ElasticsearchSearchEngineInformation.java
@@ -16,13 +16,28 @@ package com.liferay.portal.search.elasticsearch6.internal.information;
 
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.search.elasticsearch6.internal.ElasticsearchSearchEngine;
 import com.liferay.portal.search.elasticsearch6.internal.connection.ElasticsearchConnection;
 import com.liferay.portal.search.elasticsearch6.internal.connection.ElasticsearchConnectionManager;
 import com.liferay.portal.search.elasticsearch6.internal.connection.OperationMode;
 import com.liferay.portal.search.engine.SearchEngineInformation;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.elasticsearch.Version;
+import org.elasticsearch.action.admin.cluster.node.info.NodeInfo;
+import org.elasticsearch.action.admin.cluster.node.info.NodesInfoRequestBuilder;
+import org.elasticsearch.action.admin.cluster.node.info.NodesInfoResponse;
+import org.elasticsearch.client.AdminClient;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.ClusterAdminClient;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.unit.TimeValue;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -36,7 +51,7 @@ public class ElasticsearchSearchEngineInformation
 
 	@Override
 	public String getStatusString() {
-		StringBundler sb = new StringBundler(4);
+		StringBundler sb = new StringBundler(9);
 
 		sb.append(elasticsearchSearchEngine.getVendor());
 		sb.append(CharPool.SPACE);
@@ -49,6 +64,51 @@ public class ElasticsearchSearchEngineInformation
 				OperationMode.EMBEDDED) {
 
 			sb.append(" (embedded)");
+
+			return sb.toString();
+		}
+
+		try {
+			Client client = elasticsearchConnectionManager.getClient();
+
+			if (client == null) {
+				return sb.toString();
+			}
+
+			List<NodeInfo> nodesInfo = _getClusterNodes(client);
+
+			sb.append(" (cluster version: ");
+
+			NodeInfo firstNodeInfo = nodesInfo.get(0);
+
+			DiscoveryNode firstNode = firstNodeInfo.getNode();
+
+			sb.append(firstNode.getVersion());
+
+			sb.append(", nodes: ");
+
+			Stream<NodeInfo> stream = nodesInfo.stream();
+
+			String nodes = stream.map(
+				nodeInfo -> {
+					DiscoveryNode node = nodeInfo.getNode();
+
+					return node.getName();
+				}
+			).collect(
+				Collectors.joining(StringPool.COMMA_AND_SPACE)
+			);
+
+			sb.append(nodes);
+
+			sb.append(")");
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+
+			sb.append(" (error: ");
+			sb.append(e.toString());
+			sb.append(")");
 		}
 
 		return sb.toString();
@@ -59,5 +119,24 @@ public class ElasticsearchSearchEngineInformation
 
 	@Reference
 	protected ElasticsearchSearchEngine elasticsearchSearchEngine;
+
+	private List<NodeInfo> _getClusterNodes(Client client) {
+		AdminClient adminClient = client.admin();
+
+		ClusterAdminClient clusterAdminClient = adminClient.cluster();
+
+		NodesInfoRequestBuilder nodesInfoRequestBuilder =
+			clusterAdminClient.prepareNodesInfo();
+
+		TimeValue timeout = TimeValue.timeValueMillis(10000);
+
+		NodesInfoResponse nodesInfoResponse = nodesInfoRequestBuilder.get(
+			timeout);
+
+		return nodesInfoResponse.getNodes();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ElasticsearchSearchEngineInformation.class);
 
 }
