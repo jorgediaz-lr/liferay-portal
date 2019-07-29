88e20b536a532e3e1ba5e283fa03ba44ac20ce01
Jorge Díaz <jorge.diaz@liferay.com>
2019-07-29 03:55:27 +0200

    LPS-98519 Prepend path context in case URI starts with slash and it is missing

--- portal-impl/src/com/liferay/portal/util/PortalImpl.java
+++ portal-impl/src/com/liferay/portal/util/PortalImpl.java
@@ -5283,7 +5283,13 @@ public class PortalImpl implements Portal {
 			parameterMap = HttpUtil.getParameterMap(queryString);
 		}
 
-		StringBundler sb = new StringBundler(17);
+		StringBundler sb = new StringBundler(18);
+
+		if (uri.startsWith(StringPool.FORWARD_SLASH) &&
+			!uri.startsWith(getPathContext())) {
+
+			sb.append(getPathContext());
+		}
 
 		// URI
 
