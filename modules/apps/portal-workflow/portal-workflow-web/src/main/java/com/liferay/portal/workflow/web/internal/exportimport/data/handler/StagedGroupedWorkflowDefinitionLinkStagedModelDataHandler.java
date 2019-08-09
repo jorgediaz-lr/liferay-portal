3594f0909ba1d41eb66e8b55b4fe9860085720a0
Jorge Díaz <jorge.diaz@liferay.com>
2019-08-09 17:10:36 +0200

    LPS-99287 Use the DDMStructure uuid during export/import operations instead of typePK (that stores structureId)

--- modules/apps/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/exportimport/data/handler/StagedGroupedWorkflowDefinitionLinkStagedModelDataHandler.java
+++ modules/apps/portal-workflow/portal-workflow-web/src/main/java/com/liferay/portal/workflow/web/internal/exportimport/data/handler/StagedGroupedWorkflowDefinitionLinkStagedModelDataHandler.java
@@ -14,13 +14,18 @@
 
 package com.liferay.portal.workflow.web.internal.exportimport.data.handler;
 
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
 import com.liferay.exportimport.kernel.lar.BaseStagedModelDataHandler;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.PortletDataException;
 import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandlerUtil;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.adapter.StagedGroupedWorkflowDefinitionLink;
 import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalService;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.xml.Element;
 
 import java.util.Collections;
@@ -92,6 +97,33 @@ public class StagedGroupedWorkflowDefinitionLinkStagedModelDataHandler
 			String.valueOf(
 				stagedGroupedWorkflowDefinitionLink.
 					getWorkflowDefinitionVersion()));
+
+		long typePK = stagedGroupedWorkflowDefinitionLink.getTypePK();
+
+		element.addAttribute("type-pk", String.valueOf(typePK));
+
+		if (typePK == -1) {
+			return;
+		}
+
+		DDMStructure ddmStructure = _ddmStructureLocalService.fetchStructure(
+			typePK);
+
+		if (ddmStructure == null) {
+			return;
+		}
+
+		StagedModelDataHandlerUtil.exportReferenceStagedModel(
+			portletDataContext, stagedGroupedWorkflowDefinitionLink,
+			ddmStructure, PortletDataContext.REFERENCE_TYPE_DEPENDENCY);
+
+		element.addAttribute(
+			"type-uuid",
+			StringUtil.merge(
+				new Object[] {
+					ddmStructure.getUuid(), ddmStructure.getGroupId()
+				},
+				StringPool.POUND));
 	}
 
 	@Override
@@ -107,6 +139,9 @@ public class StagedGroupedWorkflowDefinitionLinkStagedModelDataHandler
 				stagedGroupedWorkflowDefinitionLink)
 		throws Exception {
 	}
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
 
 	@Reference
 	private WorkflowDefinitionLinkLocalService
