0532f455b68c7aa1a47f94dca2f8e29619b68a9c
Andrés Fulla <andres.fulla@liferay.com>
2019-10-02 13:30:05 +0200

    LPS-102337 Cleanning after navigation via front

--- modules/dxp/apps/segments/segments-experiment-web/src/main/resources/META-INF/resources/js/util/navigation.es.js
+++ modules/dxp/apps/segments/segments-experiment-web/src/main/resources/META-INF/resources/js/util/navigation.es.js
@@ -9,8 +9,14 @@
  * distribution rights of the Software.
  */
 
+const EXPERIENCE_ID_URL_KEY = 'segmentsExperienceId';
+
+const EXPERIENCE_KEY_URL_KEY = 'segmentsExperienceKey';
+const EXPERIMENT_KEY_URL_KEY = 'segmentsExperimentKey';
+
 /**
  * Generates standard navigation between Experiences
+ * Cleans alternative queryParams externally used to navigate in Experiments
  *
  * @export
  * @param {string} experienceId
@@ -24,7 +30,10 @@ export function navigateToExperience(
 	const urlQueryString = currentUrl.search;
 	const urlSearchParams = new URLSearchParams(urlQueryString);
 
-	urlSearchParams.set('segmentsExperienceId', experienceId);
+	urlSearchParams.delete(EXPERIENCE_KEY_URL_KEY);
+	urlSearchParams.delete(EXPERIMENT_KEY_URL_KEY);
+
+	urlSearchParams.set(EXPERIENCE_ID_URL_KEY, experienceId);
 	currentUrl.search = urlSearchParams.toString();
 
 	const newUrl = currentUrl.toString();
