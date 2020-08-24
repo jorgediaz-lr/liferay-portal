112b3e1ee4ea882175b0c021a7f151c8771854fa
Richard Jeremias <ricmartinsrocha@gmail.com>
2020-08-24 12:23:32 -0300

    LPS-118120 build services

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/service/DDMFormInstanceServiceWrapper.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/service/DDMFormInstanceServiceWrapper.java
@@ -124,6 +124,20 @@ public class DDMFormInstanceServiceWrapper
 	@Override
 	public java.util.List
 		<com.liferay.dynamic.data.mapping.model.DDMFormInstance> search(
+			long companyId, long groupId, String keywords, int status,
+			int start, int end,
+			com.liferay.portal.kernel.util.OrderByComparator
+				<com.liferay.dynamic.data.mapping.model.DDMFormInstance>
+					orderByComparator) {
+
+		return _ddmFormInstanceService.search(
+			companyId, groupId, keywords, status, start, end,
+			orderByComparator);
+	}
+
+	@Override
+	public java.util.List
+		<com.liferay.dynamic.data.mapping.model.DDMFormInstance> search(
 			long companyId, long groupId, String keywords, int start, int end,
 			com.liferay.portal.kernel.util.OrderByComparator
 				<com.liferay.dynamic.data.mapping.model.DDMFormInstance>
@@ -151,6 +165,14 @@ public class DDMFormInstanceServiceWrapper
 	public int searchCount(long companyId, long groupId, String keywords) {
 		return _ddmFormInstanceService.searchCount(
 			companyId, groupId, keywords);
+	}
+
+	@Override
+	public int searchCount(
+		long companyId, long groupId, String keywords, int status) {
+
+		return _ddmFormInstanceService.searchCount(
+			companyId, groupId, keywords, status);
 	}
 
 	@Override
