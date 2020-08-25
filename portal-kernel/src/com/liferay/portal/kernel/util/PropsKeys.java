f0e89d2e925693959467ea268190b20746396d35
Carlos Sierra Andrés <carlos.sierra@liferay.com>
2020-08-25 13:13:15 +0200

    LPS-115713 Only serialize when in developer mode

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -1703,6 +1703,9 @@ public interface PropsKeys {
 	public static final String JSON_SERVICE_INVALID_METHOD_NAMES =
 		"json.service.invalid.method.names";
 
+	public static final String JSON_SERVICE_SERIALIZE_THROWABLES =
+		"json.service.serialize.throwables";
+
 	public static final String JSON_WEB_SERVICE_ENABLED =
 		"json.web.service.enabled";
 
