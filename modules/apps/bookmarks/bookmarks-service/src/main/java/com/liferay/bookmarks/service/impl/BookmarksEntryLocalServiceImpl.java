6ecd4d21f6a68d440d7894abbecffa0900697bd6
Róbert Gusztus <robert.gusztus@liferay.com>
2020-08-31 10:01:04 +0200

    LPS-120071 Delete Expando Values for the class only

--- modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/service/impl/BookmarksEntryLocalServiceImpl.java
+++ modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/service/impl/BookmarksEntryLocalServiceImpl.java
@@ -26,12 +26,15 @@ import com.liferay.bookmarks.model.BookmarksFolderConstants;
 import com.liferay.bookmarks.service.base.BookmarksEntryLocalServiceBaseImpl;
 import com.liferay.bookmarks.social.BookmarksActivityKeys;
 import com.liferay.bookmarks.util.comparator.EntryModifiedDateComparator;
+import com.liferay.expando.kernel.model.ExpandoRow;
+import com.liferay.expando.kernel.model.ExpandoTableConstants;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery;
 import com.liferay.portal.kernel.dao.orm.Property;
 import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.dao.orm.RestrictionsFactoryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
@@ -199,7 +202,16 @@ public class BookmarksEntryLocalServiceImpl
 
 		// Expando
 
-		expandoRowLocalService.deleteRows(entry.getEntryId());
+		List<ExpandoRow> rows = expandoRowLocalService.getRows(
+			entry.getCompanyId(), BookmarksEntry.class.getName(),
+			ExpandoTableConstants.DEFAULT_TABLE_NAME, QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS);
+
+		for (ExpandoRow row : rows) {
+			if (row.getClassPK() == entry.getEntryId()) {
+				expandoRowLocalService.deleteRow(row);
+			}
+		}
 
 		// Ratings
 
