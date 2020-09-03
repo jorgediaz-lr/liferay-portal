126c03ceb035cb61f963d4f5794f31f76222ff82
Laszlo Pap <laszlo.pap@liferay.com>
2020-09-03 22:55:05 +0200

    LPS-120292 Register Upgrade step

--- portal-impl/src/com/liferay/portal/upgrade/v7_2_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_2_x/PortalUpgradeProcessRegistryImpl.java
@@ -22,7 +22,7 @@ import com.liferay.portal.upgrade.util.PortalUpgradeProcessRegistry;
 import java.util.TreeMap;
 
 /**
- * @author José Ángel Jiménez
+ * @author JosĂ© Ă�ngel JimĂ©nez
  */
 public class PortalUpgradeProcessRegistryImpl
 	implements PortalUpgradeProcessRegistry {
@@ -51,6 +51,8 @@ public class PortalUpgradeProcessRegistryImpl
 		upgradeProcesses.put(new Version(5, 1, 0), new UpgradeMVCCVersion());
 
 		upgradeProcesses.put(new Version(5, 1, 1), new DummyUpgradeProcess());
+
+		upgradeProcesses.put(new Version(5, 1, 2), new UpgradeLayoutDescription());
 	}
 
 }
