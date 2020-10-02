52c1e5a871265b4aef1640bcb5511e36ce72ca15
Marcos <marcos.martins@liferay.com>
2020-10-02 15:38:45 -0300

    LPS-120149 extract a more generic function to get a property label from a field

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleList/RuleList.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleList/RuleList.es.js
@@ -210,20 +210,24 @@ class RuleList extends Component {
 	}
 
 	_getOptionLabel(fieldName, optionValue) {
+		return this._getPropertyLabel(fieldName, 'options', optionValue);
+	}
+
+	_getPropertyLabel(fieldName, propertyName, propertyValue) {
 		const pages = this.pages;
 
 		let fieldLabel = null;
 
-		if (pages && optionValue) {
+		if (pages && propertyValue) {
 			const visitor = new PagesVisitor(pages);
 
 			visitor.findField(field => {
 				let found = false;
 
 				if (field.fieldName === fieldName && field.options) {
-					field.options.some(option => {
-						if (option.value == optionValue) {
-							fieldLabel = option.label;
+					field[propertyName].some(property => {
+						if (property.value == propertyValue) {
+							fieldLabel = property.label;
 
 							found = true;
 						}
@@ -236,7 +240,7 @@ class RuleList extends Component {
 			});
 		}
 
-		return fieldLabel;
+		return fieldLabel ? fieldLabel : propertyValue;
 	}
 
 	_getRulesCardOptions(rule) {
