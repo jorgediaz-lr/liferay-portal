39e7b380a54e467f0e94592d7dcf84a0f719b9d7
Marcos <marcos.martins@liferay.com>
2020-10-02 15:21:19 -0300

    LPS-120149 pass rows and columns attributes to soy template

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-builder/src/main/resources/META-INF/resources/js/components/RuleEditor/RuleEditor.es.js
@@ -310,7 +310,9 @@ class RuleEditor extends Component {
 
 		const conditions = state.conditions.map(condition => {
 			const fieldName = condition.operands[0].value;
+			let firstOperandColumns = [];
 			let firstOperandOptions = [];
+			let firstOperandRows = [];
 			let operators = [];
 
 			if (fieldName) {
@@ -318,15 +320,36 @@ class RuleEditor extends Component {
 
 				operators = this._getOperatorsByFieldType(dataType);
 
+				const {columns, rows} = this.getFieldColumnsAndRows(fieldName);
+
+				firstOperandColumns = columns;
+				firstOperandRows = rows;
+
 				firstOperandOptions = this.getFieldOptions(fieldName);
 			}
 
 			return {
 				...condition,
 				binaryOperator: this._isBinary(condition.operator),
+				firstOperandColumns,
 				firstOperandOptions,
+				firstOperandRows,
 				operands: condition.operands.map((operand, index) => {
 					if (index === 1 && this.isValueOperand(operand)) {
+						const firstOperandType = getFieldProperty(
+							pages,
+							condition.operands[0].value,
+							'type'
+						);
+
+						if (
+							firstOperandType === 'grid' &&
+							!!operand.value &&
+							typeof operand.value == 'string'
+						) {
+							operand.value = JSON.parse(operand.value);
+						}
+
 						operand = {
 							...operand,
 							dataType: getFieldProperty(
@@ -334,11 +357,7 @@ class RuleEditor extends Component {
 								condition.operands[0].value,
 								'dataType'
 							),
-							type: getFieldProperty(
-								pages,
-								condition.operands[0].value,
-								'type'
-							)
+							type: firstOperandType
 						};
 					}
 
