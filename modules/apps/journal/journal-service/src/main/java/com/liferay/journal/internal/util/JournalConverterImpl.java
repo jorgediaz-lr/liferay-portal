a7d6b95532bf7e7261c678cd79eaa90df4e01ac4
Daniel Couso <daniel.couso@liferay.com>
2020-10-06 13:52:32 +0200

    LPS-121208 Also apply titleMap here to show the current title in the Web Content field

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/util/JournalConverterImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/util/JournalConverterImpl.java
@@ -691,6 +691,8 @@ public class JournalConverterImpl implements JournalConverter {
 						jsonObject.put(
 							"title", title
 						).put(
+							"titleMap", article.getTitleMap()
+						).put(
 							"uuid", article.getUuid()
 						);
 					}
