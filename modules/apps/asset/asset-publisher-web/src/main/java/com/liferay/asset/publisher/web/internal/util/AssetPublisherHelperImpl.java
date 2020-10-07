83168e1ece8441620faf45820015d83817265127
Attila Bakay <attila.bakay@liferay.com>
2020-10-07 11:52:48 +0200

    LPS-121757 adding queryValues to allAssetTagNames instead of override

--- modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
+++ modules/apps/asset/asset-publisher-web/src/main/java/com/liferay/asset/publisher/web/internal/util/AssetPublisherHelperImpl.java
@@ -488,7 +488,7 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 
 	@Override
 	public String[] getAssetTagNames(PortletPreferences portletPreferences) {
-		String[] allAssetTagNames = new String[0];
+		List<String> allAssetTagNames = new ArrayList<>();
 
 		for (int i = 0; true; i++) {
 			String[] queryValues = portletPreferences.getValues(
@@ -510,11 +510,11 @@ public class AssetPublisherHelperImpl implements AssetPublisherHelper {
 			if (Objects.equals(queryName, "assetTags") && queryContains &&
 				(queryAndOperator || (queryValues.length == 1))) {
 
-				allAssetTagNames = queryValues;
+				Collections.addAll(allAssetTagNames, queryValues);
 			}
 		}
 
-		return allAssetTagNames;
+		return allAssetTagNames.toArray(new String[0]);
 	}
 
 	@Override
