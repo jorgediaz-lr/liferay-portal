a16a24e83b87a8ad565abc92ff7095d8efe31b0c
Brian Chan <brian.chan@liferay.com>
2020-10-15 11:06:49 -0700

    LPS-121809 SF

--- portal-impl/src/com/liferay/portal/service/persistence/impl/UserFinderImpl.java
+++ portal-impl/src/com/liferay/portal/service/persistence/impl/UserFinderImpl.java
@@ -1311,7 +1311,6 @@ public class UserFinderImpl extends UserFinderBaseImpl implements UserFinder {
 				params3 = new LinkedHashMap<>(params1);
 
 				params3.remove("socialRelationType");
-
 				params3.remove("usersGroups");
 
 				params3.put("groupsUserGroups", groupIds);
