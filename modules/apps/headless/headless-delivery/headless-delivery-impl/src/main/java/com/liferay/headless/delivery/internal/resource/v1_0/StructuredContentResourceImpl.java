4e5ea557c5840aa951caa50811e2fc50edb03eab
Javier de Arcos <javier.dearcos@liferay.com>
2020-10-19 18:43:26 +0200

    LPS-122018 Fix replacing a Structured Content with repeatable fields through Headless APIs

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/StructuredContentResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/StructuredContentResourceImpl.java
@@ -460,7 +460,8 @@ public class StructuredContentResourceImpl
 				_journalConverter.getContent(
 					ddmStructure,
 					_toFields(
-						structuredContent.getContentFields(), journalArticle)),
+						structuredContent.getContentFields(), journalArticle),
+					journalArticle.getGroupId()),
 				journalArticle.getDDMStructureKey(),
 				_getDDMTemplateKey(ddmStructure),
 				journalArticle.getLayoutUuid(),
@@ -829,9 +830,6 @@ public class StructuredContentResourceImpl
 
 		DDMStructure ddmStructure = journalArticle.getDDMStructure();
 
-		Fields fields = _journalConverter.getDDMFields(
-			ddmStructure, journalArticle.getContent());
-
 		ServiceContext serviceContext = new ServiceContext();
 
 		DDMFormValues ddmFormValues = DDMFormValuesUtil.toDDMFormValues(
@@ -842,22 +840,7 @@ public class StructuredContentResourceImpl
 
 		serviceContext.setAttribute("ddmFormValues", _toString(ddmFormValues));
 
-		Fields newFields = _ddm.getFields(
-			ddmStructure.getStructureId(), serviceContext);
-
-		Iterator<Field> iterator = fields.iterator();
-
-		while (iterator.hasNext()) {
-			Field field = iterator.next();
-
-			Field newField = newFields.get(field.getName());
-
-			field.setValues(
-				contextAcceptLanguage.getPreferredLocale(),
-				newField.getValues(contextAcceptLanguage.getPreferredLocale()));
-		}
-
-		return fields;
+		return _ddm.getFields(ddmStructure.getStructureId(), serviceContext);
 	}
 
 	private Fields _toPatchedFields(
