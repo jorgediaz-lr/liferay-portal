6963c70d3a176b4c4b39e2112e3a8a67e5a63a60
jesseyeh-liferay <jesse.yeh@liferay.com>
2020-10-21 15:22:11 -0700

    LPS-122484 Add additional system properties to prevent irrelevant logs from appearing when forking a process
    Through static initializers in the corresponding classes, irrelevant logs appear depending on the type of forked process. The following system properties are used to prevent such logs:
    
    * configuration.impl.quiet=true suppresses portal.properties reload logs
    * server.detector.quiet=true    suppresses ServerDetector's server type detection logs
    * system.properties.quiet=true  suppresses system.properties reload logs
    
    Original commit: 7845fccbb3ae6

--- portal-impl/src/com/liferay/portal/configuration/ConfigurationImpl.java
+++ portal-impl/src/com/liferay/portal/configuration/ConfigurationImpl.java
@@ -23,6 +23,7 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PropertiesUtil;
 import com.liferay.portal.kernel.util.Validator;
 
@@ -49,6 +50,9 @@ import org.apache.commons.configuration.MapConfiguration;
  */
 public class ConfigurationImpl
 	implements com.liferay.portal.kernel.configuration.Configuration {
+
+	public static final String CONFIGURATION_IMPL_QUIET =
+		"configuration.impl.quiet";
 
 	/**
 	 * @deprecated As of Judson (7.1.x), replaced by {@link
@@ -344,6 +348,12 @@ public class ConfigurationImpl
 	}
 
 	protected void printSources(long companyId, String webId) {
+		if (GetterUtil.getBoolean(
+				System.getProperty(CONFIGURATION_IMPL_QUIET))) {
+
+			return;
+		}
+
 		List<String> sources = _classLoaderAggregateProperties.loadedSources();
 
 		for (int i = sources.size() - 1; i >= 0; i--) {
