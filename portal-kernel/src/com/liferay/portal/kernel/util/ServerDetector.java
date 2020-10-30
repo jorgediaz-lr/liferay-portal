5fb99784bc59d321f5734238db14642ae033d1a8
shuyangzhou <shuyang.zhou@liferay.com>
2020-10-30 12:56:43 -0700

    LPS-122914 Optimize ServerDetector to avoid runtime server name to lower case conversion

--- portal-kernel/src/com/liferay/portal/kernel/util/ServerDetector.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/ServerDetector.java
@@ -77,7 +77,7 @@ public class ServerDetector {
 	}
 
 	public static String getServerId() {
-		return StringUtil.toLowerCase(_serverType.toString());
+		return _serverType.getLowerCaseName();
 	}
 
 	/**
@@ -326,17 +326,26 @@ public class ServerDetector {
 
 		if (System.getProperty("external-properties") == null) {
 			if (_log.isInfoEnabled()) {
-				_log.info(
-					"Detected server " +
-						StringUtil.toLowerCase(_serverType.toString()));
+				_log.info("Detected server " + _serverType.getLowerCaseName());
 			}
 		}
 	}
 
 	private enum ServerType {
 
-		GLASSFISH, JBOSS, JETTY, JONAS, OC4J, RESIN, TOMCAT, UNKNOWN, WEBLOGIC,
-		WEBSPHERE, WILDFLY
+		GLASSFISH("glassfish"), JBOSS("jboss"), JETTY("jetty"), JONAS("jonas"),
+		OC4J("oc4j"), RESIN("resin"), TOMCAT("tomcat"), UNKNOWN("unknown"),
+		WEBLOGIC("weblogic"), WEBSPHERE("websphere"), WILDFLY("wildfly");
+
+		public String getLowerCaseName() {
+			return _lowerCaseName;
+		}
+
+		private ServerType(String lowerCaseName) {
+			_lowerCaseName = lowerCaseName;
+		}
+
+		private final String _lowerCaseName;
 
 	}
 
