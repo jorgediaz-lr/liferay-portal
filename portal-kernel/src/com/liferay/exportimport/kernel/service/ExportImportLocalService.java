bfe1432307a0b55f15c1ca156fcf22429870f507
Tamas Molnar <tamas.molnar@liferay.com>
2020-11-02 10:04:31 +0100

    LPS-121138 Autogenerated - ant build-service-portlet-exportimpor

--- portal-kernel/src/com/liferay/exportimport/kernel/service/ExportImportLocalService.java
+++ portal-kernel/src/com/liferay/exportimport/kernel/service/ExportImportLocalService.java
@@ -113,6 +113,11 @@ public interface ExportImportLocalService extends BaseLocalService {
 			ExportImportConfiguration exportImportConfiguration, File file)
 		throws PortalException;
 
+	public long importLayoutSetPrototypeInBackground(
+			long userId, ExportImportConfiguration exportImportConfiguration,
+			File file)
+		throws PortalException;
+
 	public long importLayoutsInBackground(
 			long userId, ExportImportConfiguration exportImportConfiguration,
 			File file)
