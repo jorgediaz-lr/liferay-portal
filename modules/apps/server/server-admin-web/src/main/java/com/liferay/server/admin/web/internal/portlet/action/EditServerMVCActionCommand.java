d1131670c7d9bc855e0e1cabf6786f61d2c03383
Brian Chan <brian.chan@liferay.com>
2020-11-05 11:31:17 -0800

    LPS-122726 Sort alphabetically (which also happens to match the rest of the JSP

--- modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/EditServerMVCActionCommand.java
+++ modules/apps/server/server-admin-web/src/main/java/com/liferay/server/admin/web/internal/portlet/action/EditServerMVCActionCommand.java
@@ -502,8 +502,8 @@ public class EditServerMVCActionCommand extends BaseMVCActionCommand {
 		throws Exception {
 
 		String language = ParamUtil.getString(actionRequest, "language");
-		String script = ParamUtil.getString(actionRequest, "script");
 		String output = ParamUtil.getString(actionRequest, "output");
+		String script = ParamUtil.getString(actionRequest, "script");
 
 		PortletConfig portletConfig = getPortletConfig(actionRequest);
 
