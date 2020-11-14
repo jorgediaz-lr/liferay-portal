b435a73ac88955f190f7dc2efa637c02f5ad24d7
Lianne Louie <lianne.louie@liferay.com>
2020-11-13 20:41:48 -0800

    LPS-122009 - properly replace structure ID when referring to fields in the orderBy fields

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/exportimport/content/processor/AssetListEntryExportImportContentProcessor.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/exportimport/content/processor/AssetListEntryExportImportContentProcessor.java
@@ -35,6 +35,7 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringBundler;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
@@ -245,6 +246,10 @@ public class AssetListEntryExportImportContentProcessor
 			AssetRendererFactoryRegistryUtil.getAssetRendererFactories(
 				portletDataContext.getCompanyId());
 
+		Map<Long, Long> ddmStructureIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				DDMStructure.class);
+
 		for (AssetRendererFactory assetRendererFactory :
 				assetRendererFactories) {
 
@@ -262,9 +267,6 @@ public class AssetListEntryExportImportContentProcessor
 
 			LongStream classTypeIdsStream = Arrays.stream(classTypeIds);
 
-			Map<Long, Long> ddmStructureIds =
-				(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
-					DDMStructure.class);
 			Map<Long, Long> dlFileEntryTypeIds =
 				(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
 					DLFileEntryType.class);
@@ -295,9 +297,10 @@ public class AssetListEntryExportImportContentProcessor
 
 		for (Map.Entry<String, String> entry : unicodeProperties.entrySet()) {
 			String key = entry.getKey();
+			String value = entry.getValue();
 
 			if (StringUtil.startsWith(key, "queryName") &&
-				Objects.equals(entry.getValue(), "assetCategories")) {
+				Objects.equals(value, "assetCategories")) {
 
 				String index = key.substring(9);
 
@@ -323,6 +326,24 @@ public class AssetListEntryExportImportContentProcessor
 
 				unicodeProperties.setProperty(
 					"queryValues" + index, StringUtil.merge(newCategoryIds));
+			}
+
+			if (StringUtil.startsWith(key, "orderByColumn") &&
+				StringUtil.startsWith(value, "ddm__keyword__")) {
+
+				Long oldId = Long.valueOf(value.substring(14, 19));
+
+				if (ddmStructureIds.containsKey(oldId)) {
+					Long newStructureId = ddmStructureIds.get(oldId);
+
+					StringBundler sb = new StringBundler(3);
+
+					sb.append(value.substring(0, 14));
+					sb.append(newStructureId);
+					sb.append(value.substring(19));
+
+					unicodeProperties.setProperty(key, sb.toString());
+				}
 			}
 		}
 
