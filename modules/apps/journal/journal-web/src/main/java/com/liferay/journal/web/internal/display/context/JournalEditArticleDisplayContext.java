6b00e442edaac081d7ef30ef9b0f00218173ec96
balazssk <balazs.safrany-kovalik@liferay.com>
2020-11-30 08:27:14 +0100

    LPS-123530 Copy the Immutable availableLocales

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
@@ -57,6 +57,7 @@ import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
@@ -127,7 +128,7 @@ public class JournalEditArticleDisplayContext {
 			"default",
 			LanguageUtil.format(_httpServletRequest, "default", "content"));
 
-		Set<Locale> locales = getAvailableLocales();
+		Set<Locale> locales = new HashSet<>(getAvailableLocales());
 
 		locales.add(LocaleUtil.fromLanguageId(getDefaultArticleLanguageId()));
 
