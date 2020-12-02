b20438d91ccf7b3ba6f31006565a9ebf73eee735
Roland Pákai <roland.pakai@liferay.com>
2020-12-02 13:44:01 +0100

    LPS-123371 Handle if selectedData is undefined

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/util.js
@@ -1672,7 +1672,7 @@
 					}
 
 					var disabled =
-						selectedData && selectedData.includes(assetEntryId);
+						!!selectedData && selectedData.includes(assetEntryId);
 
 					if (disabled) {
 						item.attr('data-prevent-selection', true);
