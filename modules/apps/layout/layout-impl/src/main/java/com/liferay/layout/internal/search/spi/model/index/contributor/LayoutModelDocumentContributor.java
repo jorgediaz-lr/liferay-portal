2ec5def67c967498416e0a6f842a3bd7be02f542
Pavel Savinov <pavel.savinov@liferay.com>
2020-12-03 14:42:13 +0100

    LPS-124407 Remove method, we don't use it anymore

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/spi/model/index/contributor/LayoutModelDocumentContributor.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/spi/model/index/contributor/LayoutModelDocumentContributor.java
@@ -14,7 +14,6 @@
 
 package com.liferay.layout.internal.search.spi.model.index.contributor;
 
-import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
 import com.liferay.fragment.constants.FragmentEntryLinkConstants;
 import com.liferay.fragment.renderer.FragmentRendererController;
 import com.liferay.layout.internal.search.util.LayoutCrawler;
@@ -25,19 +24,9 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.language.LanguageUtil;
-import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.search.BooleanClause;
-import com.liferay.portal.kernel.search.BooleanClauseFactoryUtil;
-import com.liferay.portal.kernel.search.BooleanClauseOccur;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
-import com.liferay.portal.kernel.search.Hits;
-import com.liferay.portal.kernel.search.Indexer;
-import com.liferay.portal.kernel.search.IndexerRegistryUtil;
-import com.liferay.portal.kernel.search.SearchContext;
-import com.liferay.portal.kernel.service.GroupLocalService;
-import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.servlet.DynamicServletRequest;
@@ -48,7 +37,6 @@ import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
 import com.liferay.segments.constants.SegmentsExperienceConstants;
-import com.liferay.staging.StagingGroupHelper;
 
 import java.util.HashMap;
 import java.util.Locale;
@@ -158,60 +146,8 @@ public class LayoutModelDocumentContributor
 		}
 	}
 
-	private String _getStagedContent(Layout layout, Locale locale)
-		throws PortalException {
-
-		Group group = _groupLocalService.getGroup(layout.getGroupId());
-
-		Group stagingGroup = null;
-
-		if (ExportImportThreadLocal.isInitialLayoutStagingInProcess()) {
-			stagingGroup = _stagingGroupHelper.fetchLiveGroup(group);
-		}
-		else if (!group.isStaged() || group.isStagingGroup()) {
-			stagingGroup = group;
-		}
-		else {
-			stagingGroup = group.getStagingGroup();
-		}
-
-		Layout stagingLayout = _layoutLocalService.fetchLayoutByUuidAndGroupId(
-			layout.getUuid(), stagingGroup.getGroupId(),
-			layout.isPrivateLayout());
-
-		SearchContext searchContext = new SearchContext();
-
-		BooleanClause booleanClause = BooleanClauseFactoryUtil.create(
-			Field.ENTRY_CLASS_PK, String.valueOf(stagingLayout.getPlid()),
-			BooleanClauseOccur.MUST.getName());
-
-		searchContext.setBooleanClauses(new BooleanClause[] {booleanClause});
-
-		searchContext.setCompanyId(stagingGroup.getCompanyId());
-		searchContext.setEntryClassNames(new String[] {Layout.class.getName()});
-		searchContext.setGroupIds(new long[] {stagingGroup.getGroupId()});
-
-		Indexer indexer = IndexerRegistryUtil.getIndexer(
-			Layout.class.getName());
-
-		Hits hits = indexer.search(searchContext);
-
-		Document[] documents = hits.getDocs();
-
-		if (documents.length != 1) {
-			return StringPool.BLANK;
-		}
-
-		Document document = documents[0];
-
-		return document.get(Field.getLocalizedName(locale, Field.CONTENT));
-	}
-
 	@Reference
 	private FragmentRendererController _fragmentRendererController;
-
-	@Reference
-	private GroupLocalService _groupLocalService;
 
 	@Reference
 	private Html _html;
@@ -220,16 +156,10 @@ public class LayoutModelDocumentContributor
 	private LayoutCrawler _layoutCrawler;
 
 	@Reference
-	private LayoutLocalService _layoutLocalService;
-
-	@Reference
 	private LayoutPageTemplateStructureLocalService
 		_layoutPageTemplateStructureLocalService;
 
 	@Reference
 	private Portal _portal;
-
-	@Reference
-	private StagingGroupHelper _stagingGroupHelper;
 
 }
