71a46005975f438f0df1f6a9862f24d22a05623f
Brian Chan <brian.chan@liferay.com>
2020-12-09 22:39:10 -0800

    LPS-124410 Copy TaxonomyVocabularyResourceImpl.java#getAssetLibraryTaxonomyVocabulariesPage which calls getSiteTaxonomyVocabulariesPage. We assume *Site* is the main one.. we do this in all of our *ResourceImpl that have this split

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/LanguageResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/LanguageResourceImpl.java
@@ -46,23 +46,6 @@ public class LanguageResourceImpl extends BaseLanguageResourceImpl {
 
 	@Override
 	public Page<Language> getSiteLanguagesPage(Long siteId) throws Exception {
-		return _getLanguagesPage(siteId);
-	}
-
-	private Locale _getGroupDefaultLocale(long groupId) throws Exception {
-		Group group = _groupService.getGroup(groupId);
-
-		String defaultLanguageId = LocalizationUtil.getDefaultLanguageId(
-			group.getName());
-
-		if (Validator.isNotNull(defaultLanguageId)) {
-			return LocaleUtil.fromLanguageId(defaultLanguageId);
-		}
-
-		return LocaleUtil.getSiteDefault();
-	}
-
-	private Page<Language> _getLanguagesPage(Long groupId) throws Exception {
 		boolean acceptAllLanguages =
 			contextAcceptLanguage.isAcceptAllLanguages();
 
@@ -78,6 +61,19 @@ public class LanguageResourceImpl extends BaseLanguageResourceImpl {
 				availableLocale -> _toLanguage(
 					acceptAllLanguages, availableLocales, defaultLocale,
 					availableLocale, preferredLocale)));
+	}
+
+	private Locale _getGroupDefaultLocale(long groupId) throws Exception {
+		Group group = _groupService.getGroup(groupId);
+
+		String defaultLanguageId = LocalizationUtil.getDefaultLanguageId(
+			group.getName());
+
+		if (Validator.isNotNull(defaultLanguageId)) {
+			return LocaleUtil.fromLanguageId(defaultLanguageId);
+		}
+
+		return LocaleUtil.getSiteDefault();
 	}
 
 	private Language _toLanguage(
