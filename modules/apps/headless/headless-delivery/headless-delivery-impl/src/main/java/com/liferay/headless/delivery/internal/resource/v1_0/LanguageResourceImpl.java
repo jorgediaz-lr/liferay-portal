5eef511bfe473bc980b141b957058569f52d9778
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-12-10 18:56:45 +0100

    LPS-124410 Use languageTag resulting for example in en-US instead of en_US

--- modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/LanguageResourceImpl.java
+++ modules/apps/headless/headless-delivery/headless-delivery-impl/src/main/java/com/liferay/headless/delivery/internal/resource/v1_0/LanguageResourceImpl.java
@@ -78,7 +78,7 @@ public class LanguageResourceImpl extends BaseLanguageResourceImpl {
 		return new Language() {
 			{
 				countryName = locale.getDisplayCountry(preferredLocale);
-				id = LocaleUtil.toLanguageId(locale);
+				id = locale.toLanguageTag();
 				markedAsDefault = Objects.equals(defaultLocale, locale);
 				name = locale.getDisplayLanguage(preferredLocale);
 
