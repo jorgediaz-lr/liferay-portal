0a4f033217e4a1c2f230ff2d4cfe34cd165970a1
amosfong <amos.fong@liferay.com>
2020-12-21 15:58:04 -0800

    ISSUP-3353 don't automatically sync all accounts to LCS

--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-distributed-messaging/src/main/java/com/liferay/osb/provisioning/distributed/messaging/internal/subscribing/ProductPurchaseMessageSubscriber.java
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-distributed-messaging/src/main/java/com/liferay/osb/provisioning/distributed/messaging/internal/subscribing/ProductPurchaseMessageSubscriber.java
@@ -14,13 +14,17 @@
 
 package com.liferay.osb.provisioning.distributed.messaging.internal.subscribing;
 
+import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.Account;
 import com.liferay.osb.koroneiki.phloem.rest.client.dto.v1_0.ProductPurchase;
 import com.liferay.osb.koroneiki.phloem.rest.client.serdes.v1_0.ProductPurchaseSerDes;
+import com.liferay.osb.provisioning.koroneiki.web.service.AccountWebService;
 import com.liferay.osb.provisioning.lcs.web.service.LCSSubscriptionEntryWebService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -47,8 +51,13 @@ public class ProductPurchaseMessageSubscriber extends BaseMessageSubscriber {
 		ProductPurchase productPurchase = ProductPurchaseSerDes.toDTO(
 			productPurchaseJSONObject.toString());
 
-		_lcsSubscriptionEntryWebService.syncToLCS(
+		Account account = _accountWebService.getAccount(
 			productPurchase.getAccountKey());
+
+		if (_hasSyncToLCS(account)) {
+			_lcsSubscriptionEntryWebService.syncToLCS(
+				productPurchase.getAccountKey());
+		}
 	}
 
 	@Override
@@ -59,8 +68,23 @@ public class ProductPurchaseMessageSubscriber extends BaseMessageSubscriber {
 		_log.error(message, exception);
 	}
 
+	private boolean _hasSyncToLCS(Account account) throws Exception {
+		Date dateCreated = account.getDateCreated();
+
+		if (dateCreated.getTime() <= _SYNC_TO_LCS_TIME) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private static final long _SYNC_TO_LCS_TIME = 1577865600000L;
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		ProductPurchaseMessageSubscriber.class);
+
+	@Reference
+	private AccountWebService _accountWebService;
 
 	@Reference
 	private LCSSubscriptionEntryWebService _lcsSubscriptionEntryWebService;
