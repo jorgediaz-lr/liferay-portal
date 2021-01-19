ebc4fce6a8cb917182d0ef99ab4bfbf8effe2361
Brian Chan <brian.chan@liferay.com>
2021-01-19 10:57:50 -0300

    LPS-125637 I removed the new lines because I do not believe that order matters. If I'm wrong, please send a revert. Thx.

--- portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseUpgradePortletId.java
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/BaseUpgradePortletId.java
@@ -267,7 +267,6 @@ public abstract class BaseUpgradePortletId extends UpgradeProcess {
 				preferencesExpression, ", '#p_p_id_", oldRootPortletId,
 				"', '#p_p_id_", newRootPortletId, "') where portletId = '",
 				newRootPortletId, "'"));
-
 		runSQL(
 			StringBundler.concat(
 				"update PortletPreferences set preferences = replace(",
@@ -283,7 +282,6 @@ public abstract class BaseUpgradePortletId extends UpgradeProcess {
 					"_INSTANCE_', '#p_p_id_", newRootPortletId,
 					"_INSTANCE_') where portletId like '", newRootPortletId,
 					"_INSTANCE_%'"));
-
 			runSQL(
 				StringBundler.concat(
 					"update PortletPreferences set preferences = replace(",
@@ -300,7 +298,6 @@ public abstract class BaseUpgradePortletId extends UpgradeProcess {
 				"_USER_', '#p_p_id_", newRootPortletId,
 				"_USER_') where portletId like '", newRootPortletId,
 				"_USER_%'"));
-
 		runSQL(
 			StringBundler.concat(
 				"update PortletPreferences set preferences = replace(",
