c2392f9fd9a1a5abf0603d63cd386e2a435f82ae
Hugo Huijser <hugo.huijser@liferay.com>
2021-01-25 17:07:37 +0100

    LPS-105380 Rename vars

--- modules/apps/oauth2-provider/oauth2-provider-web/src/main/java/com/liferay/oauth2/provider/web/internal/display/context/OAuth2AuthorizationsManagementToolbarDisplayContext.java
+++ modules/apps/oauth2-provider/oauth2-provider-web/src/main/java/com/liferay/oauth2/provider/web/internal/display/context/OAuth2AuthorizationsManagementToolbarDisplayContext.java
@@ -83,7 +83,7 @@ public class OAuth2AuthorizationsManagementToolbarDisplayContext
 
 		String columnName = "createDate";
 
-		for (String orderByColumn : _orderByColumns) {
+		for (String orderByColumn : _ORDER_BY_COLUMNS) {
 			if (orderByCol.equals(orderByColumn)) {
 				columnName = orderByColumn;
 			}
@@ -96,7 +96,7 @@ public class OAuth2AuthorizationsManagementToolbarDisplayContext
 	private List<DropdownItem> _getOrderByDropdownItems() {
 		return new DropdownItemList() {
 			{
-				for (String orderByCol : _orderByColumns) {
+				for (String orderByCol : _ORDER_BY_COLUMNS) {
 					add(
 						dropdownItem -> {
 							dropdownItem.setActive(
@@ -113,7 +113,7 @@ public class OAuth2AuthorizationsManagementToolbarDisplayContext
 		};
 	}
 
-	private static final String[] _orderByColumns = {
+	private static final String[] _ORDER_BY_COLUMNS = {
 		"createDate", "userId", "userName", "accessTokenCreateDate",
 		"accessTokenExpirationDate", "refreshTokenCreateDate",
 		"refreshTokenExpirationDate", "remoteIPInfo"
