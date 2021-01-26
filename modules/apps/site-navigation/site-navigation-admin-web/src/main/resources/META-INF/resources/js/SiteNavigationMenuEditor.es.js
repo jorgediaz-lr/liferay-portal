d62ef5322bf2c447a47e9b7d53dbb3ffca11a203
Norbert Nemeth <norbert.nemeth@liferay.com>
2021-01-26 12:24:51 +0100

    LPS-125802 indentation should be determined by left and right movement of the dragged item

--- modules/apps/site-navigation/site-navigation-admin-web/src/main/resources/META-INF/resources/js/SiteNavigationMenuEditor.es.js
+++ modules/apps/site-navigation/site-navigation-admin-web/src/main/resources/META-INF/resources/js/SiteNavigationMenuEditor.es.js
@@ -13,7 +13,7 @@
  */
 
 import {fetch} from 'frontend-js-web';
-import {dom} from 'metal-dom';
+import {dom, toElement} from 'metal-dom';
 import {Drag, DragDrop} from 'metal-drag-drop';
 import position from 'metal-position';
 import State, {Config} from 'metal-state';
@@ -25,6 +25,7 @@ import {
 	shouldBeNested
 } from './SiteNavigationMenuDOMHandler.es';
 import {
+	MENU_CONTAINER_CLASSNAME,
 	MENU_ITEM_CLASSNAME,
 	MENU_ITEM_CONTENT_CLASSNAME,
 	MENU_ITEM_DRAG_ICON_CLASSNAME,
@@ -61,6 +62,8 @@ class SiteNavigationMenuEditor extends State {
 	 */
 	constructor(config, ...args) {
 		super(config, ...args);
+
+		this._container = toElement(`.${MENU_CONTAINER_CLASSNAME}`);
 
 		const controlMenu = document.querySelector('.control-menu');
 		this._controlMenuHeight = controlMenu ? controlMenu.offsetHeight : 0;
@@ -139,7 +142,7 @@ class SiteNavigationMenuEditor extends State {
 		const placeholderMenuItem = data.placeholder;
 		const sourceMenuItem = data.source;
 
-		const nearestMenuItem = getNearestMenuItem(
+		let nearestMenuItem = getNearestMenuItem(
 			sourceMenuItem,
 			placeholderMenuItem
 		);
@@ -166,6 +169,13 @@ class SiteNavigationMenuEditor extends State {
 				insertAtPosition(nearestMenuItem, sourceMenuItem, 0);
 			}
 			else {
+				let nearestMenuItemPosition = position.getRegion(nearestMenuItem);
+
+				while (placeholderMenuItem.offsetLeft < nearestMenuItemPosition.left && getParent(nearestMenuItem) !== this._container) {
+					nearestMenuItem = getParent(nearestMenuItem)
+					nearestMenuItemPosition = position.getRegion(nearestMenuItem);
+				}
+
 				const nearestMenuItemParent = getParent(nearestMenuItem);
 
 				const nearestMenuItemIndex =
