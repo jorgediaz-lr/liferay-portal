47081fd6ef34d207258b300378b6603e32f27f45
Roland Pákai <roland.pakai@liferay.com>
2021-02-02 14:41:54 +0100

    LPS-122758 Remove Alert warningMessage

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -723,27 +723,16 @@ AUI.add(
 					}
 
 					if (
-						!['.', ','].includes(sourceDecimal) ||
-						!['.', ','].includes(targetDecimal)
+						['.', ','].includes(sourceDecimal) &&
+						['.', ','].includes(targetDecimal)
 					) {
-						var instance = this;
-
-						var form = instance.getForm();
-
-						form.set(
-							'warningMessage',
-							Liferay.Language.get(
-								'convert-locale-numeric-values-warning-message'
-							)
-						);
+						number = number.replace(/,/g, '|');
+						number = number.replace(/\./g, ',');
+						number = number.replace(/\|/g, '.');
 
 						return number;
 					}
-
-					number = number.replace(/,/g, '|');
-					number = number.replace(/\./g, ',');
-					number = number.replace(/\|/g, '.');
-
+ 
 					return number;
 				},
 
@@ -4003,12 +3992,7 @@ AUI.add(
 				synchronousFormSubmission: {
 					validator: Lang.isBoolean,
 					value: true
-				},
-
-				warningMessage: {
-					validator: Lang.isString,
-					value: null,
-				},
+				}
 			},
 
 			AUGMENTS: [DDMPortletSupport, FieldsSupport],
@@ -4139,32 +4123,6 @@ AUI.add(
 					}
 				},
 
-				_onLocaleChanged() {
-					var instance = this;
-
-					var warningMessage = instance.get('warningMessage');
-
-					if (
-						warningMessage &&
-						warningMessage !== null &&
-						warningMessage !== ''
-					) {
-						var portletNamespace = instance.get('portletNamespace');
-
-						new A.Alert({
-							bodyContent: warningMessage,
-							boundingBox:
-								'#' + portletNamespace + 'ddmFormWarning',
-							closeable: true,
-							cssClass: 'alert-warning',
-							destroyOnHide: false,
-							render: true,
-						});
-
-						instance.set('warningMessage', '');
-					}
-				},
-
 				_updateNestedLocalizationMaps(fields) {
 					var instance = this;
 
@@ -4245,10 +4203,6 @@ AUI.add(
 							Liferay.on(
 								'inputLocalized:defaultLocaleChanged',
 								A.bind('_onDefaultLocaleChanged', instance)
-							),
-							Liferay.on(
-								'inputLocalized:localeChanged',
-								A.bind('_onLocaleChanged', instance)
 							)
 						);
 
@@ -4677,7 +4631,6 @@ AUI.add(
 	'',
 	{
 		requires: [
-			'aui-alert',
 			'aui-base',
 			'aui-color-picker-popover',
 			'aui-datatable',
