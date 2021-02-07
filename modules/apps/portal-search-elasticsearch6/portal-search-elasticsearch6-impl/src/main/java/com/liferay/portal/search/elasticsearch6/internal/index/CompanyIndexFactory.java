bcfb0e11ec3948f6c11e26689282b69bbc8e5790
Brian Chan <brian.chan@liferay.com>
2021-02-07 20:17:29 -0300

    LPS-127476 Wordsmith

--- modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/index/CompanyIndexFactory.java
+++ modules/apps/portal-search-elasticsearch6/portal-search-elasticsearch6-impl/src/main/java/com/liferay/portal/search/elasticsearch6/internal/index/CompanyIndexFactory.java
@@ -361,7 +361,7 @@ public class CompanyIndexFactory
 			(_indicesAdminClient == null)) {
 
 			if (_log.isInfoEnabled()) {
-				_log.info("Skipping processing of IndexSettingsContributor");
+				_log.info("Skipping index settings contributor");
 			}
 
 			return;
