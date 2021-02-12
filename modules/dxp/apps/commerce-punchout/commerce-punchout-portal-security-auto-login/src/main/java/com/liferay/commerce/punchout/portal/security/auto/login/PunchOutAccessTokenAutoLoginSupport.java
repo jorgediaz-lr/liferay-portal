f662937dac19a8cb6eebde629092a63e20ffe5fd
Hugo Huijser <hugo.huijser@liferay.com>
2021-02-12 11:05:57 +0100

    LPS-105380 Combine literal string and StringPool.* in plus statements

--- modules/dxp/apps/commerce-punchout/commerce-punchout-portal-security-auto-login/src/main/java/com/liferay/commerce/punchout/portal/security/auto/login/PunchOutAccessTokenAutoLoginSupport.java
+++ modules/dxp/apps/commerce-punchout/commerce-punchout-portal-security-auto-login/src/main/java/com/liferay/commerce/punchout/portal/security/auto/login/PunchOutAccessTokenAutoLoginSupport.java
@@ -54,7 +54,7 @@ public class PunchOutAccessTokenAutoLoginSupport extends BaseAutoLogin {
 			if (_log.isDebugEnabled()) {
 				_log.debug(
 					_PUNCH_OUT_ACCESS_TOKEN_PARAM +
-						"  parameter not found in " + "request");
+						"  parameter not found in request");
 			}
 
 			return null;
