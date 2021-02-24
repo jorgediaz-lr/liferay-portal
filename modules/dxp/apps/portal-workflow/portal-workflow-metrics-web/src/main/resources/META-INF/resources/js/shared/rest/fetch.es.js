9f905e852faa7a4941aea8037ea00c140cff1311
Albino Junior <albino.junior@liferay.com>
2021-02-24 16:56:03 -0300

    LPS-128294 Apply defaults before create axios instance

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/rest/fetch.es.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-web/src/main/resources/META-INF/resources/js/shared/rest/fetch.es.js
@@ -18,10 +18,6 @@ import axios from 'axios';
  * import fetch from '@/shared/rest/fetch';
  * fetch.get('/process').then(res => console.log(res));
  */
-const restClient = axios.create({
-	baseURL: '/o/portal-workflow-metrics/v1.0'
-});
-
 axios.defaults.headers.common[
 	'Accept-Language'
 ] = Liferay.ThemeDisplay.getBCP47LanguageId();
@@ -29,5 +25,9 @@ axios.defaults.headers.common[
 axios.defaults.params = {
 	['p_auth']: Liferay.authToken
 };
+
+const restClient = axios.create({
+	baseURL: '/o/portal-workflow-metrics/v1.0'
+});
 
 export default restClient;
