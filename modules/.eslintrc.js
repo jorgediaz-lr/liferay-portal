7fbdba94cc053249c14d68a2faaf0b27a3849fd1
Krešimir <kresimir.coko@liferay.com>
2021-03-19 11:07:37 +0100

    LPS-129453 Allow usage of any metal package

--- modules/.eslintrc.js
+++ modules/.eslintrc.js
@@ -34,6 +34,7 @@ config = {
 		MODULE_PATH: true
 	},
 	rules: {
+		'liferay-portal/no-metal-plugins': 'off',
 		'no-empty': ['error', {allowEmptyCatch: true}],
 		'no-for-of-loops/no-for-of-loops': 'error'
 	}
