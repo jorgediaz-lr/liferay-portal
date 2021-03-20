9bef84be96cf4e62a393cc5858cf6fca17781c6a
Brian Chan <brian.chan@liferay.com>
2021-03-20 09:23:55 -0300

    LPS-126917 FIX SF error that only happens in 7.2.x

--- modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/upgrade/v1_2_0/UpgradePortletId.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-service/src/main/java/com/liferay/portal/workflow/kaleo/internal/upgrade/v1_2_0/UpgradePortletId.java
@@ -15,7 +15,6 @@
 package com.liferay.portal.workflow.kaleo.internal.upgrade.v1_2_0;
 
 import com.liferay.portal.kernel.upgrade.BaseUpgradePortletId;
-import com.liferay.portal.workflow.web.internal.constants.WorkflowPortletKeys;
 
 /**
  * @author Sam Ziemer
@@ -25,9 +24,21 @@ public class UpgradePortletId extends BaseUpgradePortletId {
 	@Override
 	protected String[][] getRenamePortletIdsArray() {
 		return new String[][] {
-			{"151", WorkflowPortletKeys.SITE_ADMINISTRATION_WORKFLOW},
-			{"158", WorkflowPortletKeys.USER_WORKFLOW}
+			{"151", _SITE_ADMINISTRATION_WORKFLOW}, {"158", _USER_WORKFLOW}
 		};
 	}
+
+	/**
+	 * @see com.liferay.portal.workflow.web.internal.constants.WorkflowPortletKeys
+	 */
+	private static final String _SITE_ADMINISTRATION_WORKFLOW =
+		"com_liferay_portal_workflow_web_internal_portlet_" +
+			"SiteAdministrationWorkflowPortlet";
+
+	/**
+	 * @see com.liferay.portal.workflow.web.internal.constants.WorkflowPortletKeys
+	 */
+	private static final String _USER_WORKFLOW =
+		"com_liferay_portal_workflow_web_internal_portlet_UserWorkflowPortlet";
 
 }
