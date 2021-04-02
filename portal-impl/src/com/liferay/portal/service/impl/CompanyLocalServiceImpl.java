9879ef69c29168577fe8a4647a081f0636a5c2a7
pei-jung <pei-jung.lan@liferay.com>
2021-04-01 21:08:09 -0700

    LPS-129039 Clear site locale cache to reflect updated locales in language selector

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -1952,6 +1952,8 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 				}
 
 				if (updateLocales) {
+					LanguageUtil.resetAvailableGroupLocales(group.getGroupId());
+
 					groupTypeSettingsUnicodeProperties.setProperty(
 						PropsKeys.LOCALES,
 						StringUtil.merge(groupLanguageIds, StringPool.COMMA));
