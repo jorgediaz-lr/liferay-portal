9ec2fc198ceeea6a292ffa8ebdc8cd2ff3ffe5ac
Jorge Díaz <jorge.diaz@liferay.com>
2021-04-09 20:22:22 +0200

    LPS-996 Delete the Resource table entry when deleting an ExpandoColumn or an ExpandoTable

--- portal-impl/src/com/liferay/portlet/expando/service/impl/ExpandoColumnLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/expando/service/impl/ExpandoColumnLocalServiceImpl.java
@@ -27,8 +27,10 @@ import com.liferay.exportimport.kernel.lar.StagedModelType;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.adapter.ModelAdapterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
@@ -100,6 +102,18 @@ public class ExpandoColumnLocalServiceImpl
 		// Values
 
 		expandoValueLocalService.deleteColumnValues(column.getColumnId());
+
+		try {
+
+			// Resources
+
+			resourceLocalService.deleteResource(
+				column.getCompanyId(), ExpandoColumn.class.getName(),
+				ResourceConstants.SCOPE_INDIVIDUAL, column.getColumnId());
+		}
+		catch (PortalException portalException) {
+			throw new SystemException(portalException);
+		}
 	}
 
 	@Override
