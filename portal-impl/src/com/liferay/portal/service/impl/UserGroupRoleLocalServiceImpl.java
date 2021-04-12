280e943b3158c5fa5fe6f24921fe109ab6cadaa9
Jorge Díaz <jorge.diaz@liferay.com>
2021-04-12 22:48:20 +0200

    LPS-994 Force set the companyId to its group one

--- portal-impl/src/com/liferay/portal/service/impl/UserGroupRoleLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserGroupRoleLocalServiceImpl.java
@@ -48,6 +48,12 @@ public class UserGroupRoleLocalServiceImpl
 			userGroupRole.setGroupId(groupId);
 			userGroupRole.setRoleId(roleId);
 
+			Group group = groupLocalService.fetchGroup(groupId);
+
+			if (group != null) {
+				userGroupRole.setCompanyId(group.getCompanyId());
+			}
+
 			userGroupRole = userGroupRolePersistence.update(userGroupRole);
 		}
 
