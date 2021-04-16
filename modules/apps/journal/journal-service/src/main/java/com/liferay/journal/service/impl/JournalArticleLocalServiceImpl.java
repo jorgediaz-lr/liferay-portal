1601236394d73cd2a66ed7ab20ff7092cf943fbe
Jorge Díaz <jorge.diaz@liferay.com>
2021-04-16 09:55:12 +0200

    LPS-109 Avoid error when the AssetEntry record doesn't exist

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -8595,11 +8595,18 @@ public class JournalArticleLocalServiceImpl
 	protected void updatePreviousApprovedArticle(JournalArticle article)
 		throws PortalException {
 
+		AssetEntry assetEntry = assetEntryLocalService.fetchEntry(
+			JournalArticle.class.getName(), article.getResourcePrimKey());
+
+		if (assetEntry == null) {
+			updateAsset(article.getUserId(), article, null, null, null, null);
+		}
+
 		JournalArticle previousApprovedArticle = getPreviousApprovedArticle(
 			article);
 
 		if (previousApprovedArticle.getVersion() == article.getVersion()) {
-			AssetEntry assetEntry = assetEntryLocalService.updateVisible(
+			assetEntry = assetEntryLocalService.updateVisible(
 				JournalArticle.class.getName(), article.getResourcePrimKey(),
 				false);
 
@@ -8610,7 +8617,7 @@ public class JournalArticleLocalServiceImpl
 			}
 		}
 		else {
-			AssetEntry assetEntry = assetEntryLocalService.updateEntry(
+			assetEntry = assetEntryLocalService.updateEntry(
 				JournalArticle.class.getName(), article.getResourcePrimKey(),
 				previousApprovedArticle.getDisplayDate(),
 				previousApprovedArticle.getExpirationDate(),
