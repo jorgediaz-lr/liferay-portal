df5f5b51aa584418d38daca62027b083fa6644e4
Jorge Díaz <jorge.diaz@liferay.com>
2021-04-20 14:35:57 +0200

    LPS-100 Replace LocalServiceBaseImpl delete method calls with LocalServiceImpl ones in the tests

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/util/test/JournalTestUtilTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/util/test/JournalTestUtilTest.java
@@ -244,7 +244,7 @@ public class JournalTestUtilTest {
 				ddmStructure.getStructureKey(), null,
 				LocaleUtil.getSiteDefault()));
 
-		DDMStructureLocalServiceUtil.deleteDDMStructure(ddmStructure);
+		DDMStructureLocalServiceUtil.deleteStructure(ddmStructure);
 
 		try {
 			Assert.assertNull(
