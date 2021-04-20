578f3e155351b8d35ba809adbe67e48f00ec4266
Jorge Díaz <jorge.diaz@liferay.com>
2021-04-20 14:36:53 +0200

    LPS-115 Workaround: Avoid NPE in listeners

--- portal-kernel/src/com/liferay/portal/kernel/service/persistence/impl/BasePersistenceImpl.java
+++ portal-kernel/src/com/liferay/portal/kernel/service/persistence/impl/BasePersistenceImpl.java
@@ -640,6 +640,10 @@ public class BasePersistenceImpl<T extends BaseModel<T>>
 			model = modelWrapper.getWrappedModel();
 		}
 
+		if (model == null) {
+			return null;
+		}
+
 		ModelListener<T>[] listeners = getListeners();
 
 		for (ModelListener<T> listener : listeners) {
@@ -647,6 +651,10 @@ public class BasePersistenceImpl<T extends BaseModel<T>>
 		}
 
 		model = removeImpl(model);
+
+		if (model == null) {
+			return null;
+		}
 
 		for (ModelListener<T> listener : listeners) {
 			listener.onAfterRemove(model);
