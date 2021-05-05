f7a6ff22327800e13ae864a90935f26e5b8239eb
Lino Alves <lino.alves@liferay.com>
2021-05-05 13:06:34 -0300

    LPS-131711 using available function to escape label

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Select/Select.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Select/Select.es.js
@@ -249,7 +249,7 @@ class Select extends Component {
 			...option,
 			active: !multiple && included,
 			checked: multiple && included,
-			label: Liferay.Util.escapeHTML(option.label),
+			label: Liferay.Util.escape(option.label),
 			type: multiple ? 'checkbox' : 'item'
 		};
 	}
