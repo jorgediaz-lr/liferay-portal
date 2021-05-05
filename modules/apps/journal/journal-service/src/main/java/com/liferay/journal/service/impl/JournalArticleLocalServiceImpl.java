2f907938046a595a9b146f46bf5238581990e9a4
Jorge Díaz <jorge.diaz@liferay.com>
2021-05-05 19:16:33 +0200

    LPS-125 Use fetchJournalArticleResource to avoid errors when the record doesn't exist during deletion

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -1500,10 +1500,13 @@ public class JournalArticleLocalServiceImpl
 						article.getResourcePrimKey())) {
 
 					articleResource =
-						_journalArticleResourceLocalService.getArticleResource(
-							article.getResourcePrimKey());
+						_journalArticleResourceLocalService.
+							fetchJournalArticleResource(
+								article.getResourcePrimKey());
 
-					articleResources.add(articleResource);
+					if (articleResource != null) {
+						articleResources.add(articleResource);
+					}
 				}
 
 				journalArticleLocalService.deleteArticle(article, null, null);
