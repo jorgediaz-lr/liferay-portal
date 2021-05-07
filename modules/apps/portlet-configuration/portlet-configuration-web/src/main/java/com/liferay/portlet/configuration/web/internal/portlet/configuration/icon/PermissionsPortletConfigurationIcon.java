665466fc45d18f52b76699d4fe14de17cc6d029b
ChrisKian <christopher.kian@liferay.com>
2021-05-07 16:59:43 -0700

    LPS-132256 SF and less redundant checking

--- modules/apps/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
+++ modules/apps/portlet-configuration/portlet-configuration-web/src/main/java/com/liferay/portlet/configuration/web/internal/portlet/configuration/icon/PermissionsPortletConfigurationIcon.java
@@ -119,8 +119,6 @@ public class PermissionsPortletConfigurationIcon
 			portletId = portletDisplay.getPortletResource();
 		}
 
-		boolean showPermissionsIcon = false;
-
 		Layout layout = themeDisplay.getLayout();
 
 		if (!SitesUtil.isLayoutUpdateable(layout)) {
@@ -133,9 +131,12 @@ public class PermissionsPortletConfigurationIcon
 			try {
 				if (PortletPermissionUtil.contains(
 						themeDisplay.getPermissionChecker(), layout, portletId,
-						ActionKeys.PERMISSIONS)) {
+						ActionKeys.PERMISSIONS) &&
+					!layout.isLayoutPrototypeLinkActive() &&
+					!layout.isTypeControlPanel() &&
+					!isEmbeddedPersonalApplicationLayout(layout)) {
 
-					showPermissionsIcon = true;
+					return true;
 				}
 			}
 			catch (PortalException portalException) {
@@ -145,24 +146,10 @@ public class PermissionsPortletConfigurationIcon
 				if (_log.isDebugEnabled()) {
 					_log.debug(portalException, portalException);
 				}
-
-				showPermissionsIcon = false;
 			}
 		}
 
-		if (layout.isLayoutPrototypeLinkActive()) {
-			showPermissionsIcon = false;
-		}
-
-		if (layout.isTypeControlPanel()) {
-			showPermissionsIcon = false;
-		}
-
-		if (isEmbeddedPersonalApplicationLayout(layout)) {
-			showPermissionsIcon = false;
-		}
-
-		return showPermissionsIcon;
+		return false;
 	}
 
 	@Override
