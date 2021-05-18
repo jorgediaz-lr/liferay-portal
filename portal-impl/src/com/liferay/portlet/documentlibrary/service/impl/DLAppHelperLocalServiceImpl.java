674e6b488140017db9b599f3ddcf0d2eb13be857
Jorge Díaz <jorge.diaz@liferay.com>
2021-05-18 18:04:07 +0200

    LPS-131 Add missing actionableDynamicQuery.performActions() call

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppHelperLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppHelperLocalServiceImpl.java
@@ -241,6 +241,8 @@ public class DLAppHelperLocalServiceImpl
 		actionableDynamicQuery.setPerformActionMethod(
 			(DLFileEntry dlFileEntry) -> _deleteFileEntry(
 				dlFileEntry.getFileEntryId()));
+
+		actionableDynamicQuery.performActions();
 	}
 
 	@Override
