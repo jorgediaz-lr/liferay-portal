b1e50e0b16b242b9e7e19a3210d06cf7b9983e55
Jorge Díaz <jorge.diaz@liferay.com>
2021-05-31 11:39:14 +0200

    LPS-144 Avoid error in DLAppLocalServiceImpl.deleteAll in case the group doesn't exist

--- portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/documentlibrary/service/impl/DLAppLocalServiceImpl.java
@@ -546,7 +546,17 @@ public class DLAppLocalServiceImpl extends DLAppLocalServiceBaseImpl {
 	 */
 	@Override
 	public void deleteAll(long repositoryId) throws PortalException {
-		deleteRepository(getLocalRepository(repositoryId));
+		try {
+			deleteRepository(getLocalRepository(repositoryId));
+		}
+		catch (NoSuchGroupException noSuchGroupException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					noSuchGroupException, noSuchGroupException);
+			}
+
+			return;
+		}
 	}
 
 	@Override
