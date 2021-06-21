8c28b71d50111827d5dbb16e3b6e46a79e3535c9
Laszlo Pap <laszlo.pap@liferay.com>
2021-06-21 11:45:54 +0200

    LPS-127057 SF

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/service/impl/JournalArticleLocalServiceImpl.java
@@ -1162,11 +1162,6 @@ public class JournalArticleLocalServiceImpl
 		newArticle.setStatusByUserName(user.getFullName());
 		newArticle.setStatusDate(modifiedDate);
 
-		ExpandoBridgeUtil.copyExpandoBridgeAttributes(
-			oldArticle.getExpandoBridge(), newArticle.getExpandoBridge());
-
-		// Article localization
-
 		int uniqueUrlTitleCount = _getUniqueUrlTitleCount(
 			groupId, newArticleId,
 			JournalUtil.getUrlTitle(id, oldArticle.getUrlTitle()));
@@ -1204,7 +1199,12 @@ public class JournalArticleLocalServiceImpl
 
 		newArticle.setUrlTitle(newURLTitle);
 
+		ExpandoBridgeUtil.copyExpandoBridgeAttributes(
+			oldArticle.getExpandoBridge(), newArticle.getExpandoBridge());
+
 		newArticle = journalArticlePersistence.update(newArticle);
+
+		// Article localization
 
 		Map<Locale, String> friendlyURLMap = _checkFriendlyURLMap(
 			locale, new HashMap(), newTitleMap);
