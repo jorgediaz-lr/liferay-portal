9c2839a5bf1fd924ae3b317a6e45d1ee92e1f6ef
Jorge Díaz <jorge.diaz@liferay.com>
2021-07-11 20:31:57 +0200

    LPS-147 Always create the AssetEntry for User_ objects

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -1338,11 +1338,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		// Asset
 
-		if (serviceContext != null) {
-			updateAsset(
-				creatorUserId, user, serviceContext.getAssetCategoryIds(),
-				serviceContext.getAssetTagNames());
-		}
+		_updateAsset(creatorUserId, user, serviceContext);
 
 		// Indexer
 
@@ -5622,11 +5618,7 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 
 		// Asset
 
-		if (serviceContext != null) {
-			updateAsset(
-				userId, user, serviceContext.getAssetCategoryIds(),
-				serviceContext.getAssetTagNames());
-		}
+		_updateAsset(userId, user, serviceContext);
 
 		// Indexer
 
@@ -7427,6 +7419,25 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		catch (IOException ioException) {
 			throw new SystemException(ioException);
 		}
+	}
+
+	private void _updateAsset(
+			long userId, User user, ServiceContext serviceContext)
+		throws PortalException {
+
+		if (serviceContext == null) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"serviceContext is null for user " + userId,
+					new Exception());
+			}
+
+			serviceContext = new ServiceContext();
+		}
+
+		updateAsset(
+			userId, user, serviceContext.getAssetCategoryIds(),
+			serviceContext.getAssetTagNames());
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
