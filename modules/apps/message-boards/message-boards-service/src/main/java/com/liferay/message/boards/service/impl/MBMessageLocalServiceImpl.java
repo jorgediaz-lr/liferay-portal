7b61379a3dc1f783f8439b1b5cec6f3a0a02ddb7
Jorge Díaz <jorge.diaz@liferay.com>
2021-08-10 09:09:27 +0200

    LPS-151 Correctly delete the MBThread using the deleteThread instead of deleteMBThread when deleting the last MBMessage of it

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBMessageLocalServiceImpl.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/service/impl/MBMessageLocalServiceImpl.java
@@ -765,7 +765,7 @@ public class MBMessageLocalServiceImpl extends MBMessageLocalServiceBaseImpl {
 			MBThread thread = _mbThreadPersistence.findByPrimaryKey(
 				message.getThreadId());
 
-			_mbThreadLocalService.deleteMBThread(thread);
+			_mbThreadLocalService.deleteThread(thread);
 
 			// Indexer
 
