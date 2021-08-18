ed4231607e86c0817022108585b42e7c9f046d72
kylembischof <kyle.bischof@liferay.com>
2021-08-18 16:20:10 -0700

    ISSUP-4236 add license key search

---
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-rest/osb-provisioning-rest-api/src/main/java/com/liferay/osb/provisioning/rest/dto/v1_0/util/LicenseKeyUtil.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.osb.provisioning.rest.dto.v1_0.util;
+
+import com.liferay.osb.provisioning.rest.dto.v1_0.LicenseKey;
+
+/**
+ * @author Kyle Bischof
+ */
+public class LicenseKeyUtil {
+
+	public static LicenseKey toLicenseKey(
+			com.liferay.osb.provisioning.license.model.LicenseKey licenseKey)
+		throws Exception {
+
+		return new LicenseKey() {
+			{
+				accountKey = licenseKey.getAccountKey();
+				accountName = licenseKey.getAccountName();
+				active = licenseKey.getActive();
+				additionalInfo = licenseKey.getAdditionalInfo();
+				assetReceiptLicenseUuid =
+					licenseKey.getAssetReceiptLicenseUuid();
+				clusterId = licenseKey.getClusterId();
+				complimentary = licenseKey.getComplimentary();
+				createDate = licenseKey.getCreateDate();
+				description = licenseKey.getDescription();
+				expirationDate = licenseKey.getExpirationDate();
+				hostName = licenseKey.getHostName();
+				ipAddresses = licenseKey.getIpAddresses();
+				key = licenseKey.getKey();
+				licenseEntryId = licenseKey.getLicenseEntryId();
+				licenseEntryName = licenseKey.getLicenseEntryName();
+				licenseEntryType = licenseKey.getLicenseEntryType();
+				licenseKeyId = licenseKey.getLicenseKeyId();
+				licenseVersion = licenseKey.getLicenseVersion();
+				macAddresses = licenseKey.getMacAddresses();
+				maxClusterNodes = licenseKey.getMaxClusterNodes();
+				maxConcurrentUsers = licenseKey.getMaxConcurrentUsers();
+				maxHttpSessions = licenseKey.getMaxHttpSessions();
+				maxServers = licenseKey.getMaxServers();
+				maxUsers = licenseKey.getMaxUsers();
+				modifiedDate = licenseKey.getModifiedDate();
+				modifiedUserName = licenseKey.getModifiedUserName();
+				modifiedUserUuid = licenseKey.getModifiedUserUuid();
+				name = licenseKey.getName();
+				owner = licenseKey.getOwner();
+				productId = licenseKey.getProductId();
+				productKey = licenseKey.getProductKey();
+				productName = licenseKey.getProductName();
+				productPurchaseKey = licenseKey.getProductPurchaseKey();
+				productVersion = licenseKey.getProductVersion();
+				serverId = licenseKey.getServerId();
+				sizing = licenseKey.getSizing();
+				startDate = licenseKey.getStartDate();
+				userName = licenseKey.getUserName();
+				userUuid = licenseKey.getUserUuid();
+			}
+		};
+	}
+
+}
--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-rest/osb-provisioning-rest-impl/src/main/java/com/liferay/osb/provisioning/rest/internal/resource/v1_0/LicenseKeyResourceImpl.java
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-rest/osb-provisioning-rest-impl/src/main/java/com/liferay/osb/provisioning/rest/internal/resource/v1_0/LicenseKeyResourceImpl.java
@@ -14,9 +14,25 @@
 
 package com.liferay.osb.provisioning.rest.internal.resource.v1_0;
 
+import com.liferay.osb.provisioning.license.service.LicenseKeyLocalService;
+import com.liferay.osb.provisioning.rest.dto.v1_0.LicenseKey;
+import com.liferay.osb.provisioning.rest.dto.v1_0.util.LicenseKeyUtil;
+import com.liferay.osb.provisioning.rest.internal.odata.entity.v1_0.LicenseKeyEntityModel;
 import com.liferay.osb.provisioning.rest.resource.v1_0.LicenseKeyResource;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.resource.EntityModelResource;
+import com.liferay.portal.vulcan.util.SearchUtil;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -26,5 +42,37 @@ import org.osgi.service.component.annotations.ServiceScope;
 	properties = "OSGI-INF/liferay/rest/v1_0/license-key.properties",
 	scope = ServiceScope.PROTOTYPE, service = LicenseKeyResource.class
 )
-public class LicenseKeyResourceImpl extends BaseLicenseKeyResourceImpl {
+public class LicenseKeyResourceImpl
+	extends BaseLicenseKeyResourceImpl implements EntityModelResource {
+
+	@Override
+	public EntityModel getEntityModel(MultivaluedMap multivaluedMap) {
+		return _entityModel;
+	}
+
+	@Override
+	public Page<LicenseKey> getLicenseKeysPage(
+			String search, Filter filter, Pagination pagination, Sort[] sorts)
+		throws Exception {
+
+		return SearchUtil.search(
+			booleanQuery -> {
+			},
+			filter, com.liferay.osb.provisioning.license.model.LicenseKey.class,
+			search, pagination,
+			queryConfig -> queryConfig.setSelectedFieldNames(
+				Field.ENTRY_CLASS_PK),
+			searchContext -> searchContext.setCompanyId(
+				contextCompany.getCompanyId()),
+			document -> LicenseKeyUtil.toLicenseKey(
+				_licenseKeyLocalService.getLicenseKey(
+					GetterUtil.getLong(document.get(Field.ENTRY_CLASS_PK)))),
+			sorts);
+	}
+
+	private static final EntityModel _entityModel = new LicenseKeyEntityModel();
+
+	@Reference
+	private LicenseKeyLocalService _licenseKeyLocalService;
+
 }
