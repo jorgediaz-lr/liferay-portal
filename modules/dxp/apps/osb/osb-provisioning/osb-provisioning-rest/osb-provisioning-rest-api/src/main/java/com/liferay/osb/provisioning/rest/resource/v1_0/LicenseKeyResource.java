4d71cdcba89836a87719b56ac3c556a4dab34226
kylembischof <kyle.bischof@liferay.com>
2021-08-26 15:41:52 -0700

    ISSUP-4236 buildREST

--- modules/dxp/apps/osb/osb-provisioning/osb-provisioning-rest/osb-provisioning-rest-api/src/main/java/com/liferay/osb/provisioning/rest/resource/v1_0/LicenseKeyResource.java
+++ modules/dxp/apps/osb/osb-provisioning/osb-provisioning-rest/osb-provisioning-rest-api/src/main/java/com/liferay/osb/provisioning/rest/resource/v1_0/LicenseKeyResource.java
@@ -18,6 +18,8 @@ import com.liferay.osb.provisioning.rest.dto.v1_0.LicenseKey;
 import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.search.filter.Filter;
 import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
 import com.liferay.portal.vulcan.pagination.Page;
@@ -77,6 +79,12 @@ public interface LicenseKeyResource {
 
 	public void setGroupLocalService(GroupLocalService groupLocalService);
 
+	public void setResourceActionLocalService(
+		ResourceActionLocalService resourceActionLocalService);
+
+	public void setResourcePermissionLocalService(
+		ResourcePermissionLocalService resourcePermissionLocalService);
+
 	public void setRoleLocalService(RoleLocalService roleLocalService);
 
 	public static class FactoryHolder {
@@ -94,6 +102,9 @@ public interface LicenseKeyResource {
 
 		public Builder httpServletRequest(
 			HttpServletRequest httpServletRequest);
+
+		public Builder httpServletResponse(
+			HttpServletResponse httpServletResponse);
 
 		public Builder preferredLocale(Locale preferredLocale);
 
