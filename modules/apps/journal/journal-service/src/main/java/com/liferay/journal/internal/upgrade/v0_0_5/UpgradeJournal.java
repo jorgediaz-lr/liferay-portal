be589e3037419295e5b96d1f9c0a207383f497cc
Alan Huang <alan.huang@liferay.com>
2021-08-31 12:38:39 +0800

    LPS-105380 SF, use ListUtil.isEmpty

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v0_0_5/UpgradeJournal.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v0_0_5/UpgradeJournal.java
@@ -39,6 +39,7 @@ import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.upgrade.util.UpgradeProcessUtil;
 import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.LoggingTimer;
@@ -475,9 +476,7 @@ public class UpgradeJournal extends UpgradeProcess {
 	protected void transformDateFieldValues(
 		List<Element> dynamicElementElements) {
 
-		if ((dynamicElementElements == null) ||
-			dynamicElementElements.isEmpty()) {
-
+		if (ListUtil.isEmpty(dynamicElementElements)) {
 			return;
 		}
 
