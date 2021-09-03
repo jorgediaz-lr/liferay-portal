3f2fc39fe4768f892d175cdaebd6b67deb1d280c
Brian Chan <brian.chan@liferay.com>
2021-09-02 17:13:47 -0700

    LPS-138282 Regen

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-api/src/main/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/ProcessResource.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-api/src/main/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/ProcessResource.java
@@ -27,6 +27,7 @@ import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 import com.liferay.portal.workflow.metrics.rest.dto.v1_0.Process;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -105,7 +106,8 @@ public interface ProcessResource {
 	public void setRoleLocalService(RoleLocalService roleLocalService);
 
 	public default Filter toFilter(String filterString) {
-		return toFilter(filterString, null);
+		return toFilter(
+			filterString, Collections.<String, List<String>>emptyMap());
 	}
 
 	public default Filter toFilter(
--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-api/src/main/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/TaskResource.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-rest-api/src/main/java/com/liferay/portal/workflow/metrics/rest/resource/v1_0/TaskResource.java
@@ -27,6 +27,7 @@ import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 import com.liferay.portal.workflow.metrics.rest.dto.v1_0.Task;
 
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -100,7 +101,8 @@ public interface TaskResource {
 	public void setRoleLocalService(RoleLocalService roleLocalService);
 
 	public default Filter toFilter(String filterString) {
-		return toFilter(filterString, null);
+		return toFilter(
+			filterString, Collections.<String, List<String>>emptyMap());
 	}
 
 	public default Filter toFilter(
