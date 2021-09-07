e219337fe98894b5997abb35a981454db092f86a
Jorge Díaz <jorge.diaz@liferay.com>
2021-09-07 17:59:59 +0200

    LPS-138761 Delete Ticket records when deleting an user

--- portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/UserLocalServiceImpl.java
@@ -2182,6 +2182,11 @@ public class UserLocalServiceImpl extends UserLocalServiceBaseImpl {
 		socialRequestLocalService.deleteReceiverUserRequests(user.getUserId());
 		socialRequestLocalService.deleteUserRequests(user.getUserId());
 
+		// Ticket
+
+		ticketLocalService.deleteTickets(
+			user.getCompanyId(), User.class.getName(), user.getUserId());
+
 		// Mail
 
 		mailService.deleteUser(user.getCompanyId(), user.getUserId());
