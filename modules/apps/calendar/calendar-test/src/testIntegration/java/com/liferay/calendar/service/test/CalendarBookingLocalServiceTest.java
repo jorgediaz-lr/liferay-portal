d4ba88b4ba530dde784d7127fabad9e072230d4a
Selton Guedes <sfgs@ecomp.poli.br>
2021-09-17 16:57:41 -0300

    LPS-138324 Add new test to check the email notifications escaping

--- modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/service/test/CalendarBookingLocalServiceTest.java
+++ modules/apps/calendar/calendar-test/src/testIntegration/java/com/liferay/calendar/service/test/CalendarBookingLocalServiceTest.java
@@ -18,6 +18,7 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.calendar.exception.CalendarBookingRecurrenceException;
 import com.liferay.calendar.model.Calendar;
 import com.liferay.calendar.model.CalendarBooking;
+import com.liferay.calendar.model.CalendarBookingConstants;
 import com.liferay.calendar.notification.NotificationTemplateType;
 import com.liferay.calendar.notification.NotificationType;
 import com.liferay.calendar.recurrence.Recurrence;
@@ -310,7 +311,7 @@ public class CalendarBookingLocalServiceTest {
 			firstChildCalendarBooking.getEndTime(), createServiceContext());
 
 		assertSentEmail(_user);
-		assertSentEmail(invitingUser);
+		assertSentEmail(_invitingUser);
 	}
 
 	@Test
@@ -825,6 +826,54 @@ public class CalendarBookingLocalServiceTest {
 		assertCalendarBookingsCount(liveCalendar, 0);
 
 		assertCalendarBookingsCount(stagingCalendar, 0);
+	}
+
+	@Test
+	public void testEscapeEventNotificationVariables() throws Exception {
+		ServiceContext serviceContext = createServiceContext();
+
+		User invitingUser = UserTestUtil.addUser(
+			RandomTestUtil.randomString(), LocaleUtil.getDefault(), "firstN@m&",
+			"#124 & 423", new long[] {TestPropsValues.getGroupId()});
+
+		Calendar calendar = CalendarTestUtil.addCalendar(
+			invitingUser, serviceContext);
+
+		Calendar invitedCalendar = CalendarTestUtil.addCalendar(
+			_user, serviceContext);
+
+		serviceContext.setWorkflowAction(WorkflowConstants.ACTION_PUBLISH);
+
+		String mailBody =
+			"[$EVENT_TITLE|uri$], [$TO_NAME|html$], [$EVENT_LOCATION|attr$]";
+		String mailSubject = RandomTestUtil.randomString();
+
+		CalendarNotificationTemplateTestUtil.addCalendarNotificationTemplate(
+			calendar, NotificationTemplateType.INVITE, "test@liferay.com",
+			"Test Test", mailSubject, mailBody);
+
+		long startTime = System.currentTimeMillis() + (Time.MINUTE * 2);
+
+		CalendarBookingLocalServiceUtil.addCalendarBooking(
+			invitingUser.getUserId(), calendar.getCalendarId(),
+			new long[] {invitedCalendar.getCalendarId()},
+			CalendarBookingConstants.PARENT_CALENDAR_BOOKING_ID_DEFAULT,
+			CalendarBookingConstants.RECURRING_CALENDAR_BOOKING_ID_DEFAULT,
+			HashMapBuilder.put(
+				LocaleUtil.getDefault(), "mySubject #123 & 412"
+			).build(),
+			RandomTestUtil.randomLocaleStringMap(), "myLocation #123 & 321",
+			startTime, startTime + Time.HOUR, false,
+			RecurrenceSerializer.serialize(null), Time.MINUTE,
+			NotificationType.EMAIL.getValue(), 0,
+			NotificationType.EMAIL.getValue(), serviceContext);
+
+		CalendarBookingLocalServiceUtil.checkCalendarBookings();
+
+		assertMailBody(
+			mailSubject,
+			"mySubject #123 &amp; 412, firstN@m&amp; #124 &amp;" +
+				" 423, myLocation #123 &amp; 321");
 	}
 
 	@Test
