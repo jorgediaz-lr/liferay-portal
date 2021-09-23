b2edddddfa53da63b661359f6d99f2bbb5a1b58b
Norbert Nemeth <norbert.nemeth@liferay.com>
2021-09-23 14:39:46 +0200

    LPS-138165 listen on defaultLocaleChanged with onLocaleChanged handler as well

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/input_localized.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/input_localized.js
@@ -513,6 +513,10 @@ AUI.add(
 							'inputLocalized:defaultLocaleChanged',
 							A.bind('_onDefaultLocaleChanged', instance)
 						),
+						Liferay.after(
+							'inputLocalized:defaultLocaleChanged',
+							A.bind('_onLocaleChanged', instance)
+						),
 						Liferay.on(
 							'inputLocalized:localeChanged',
 							A.bind('_onLocaleChanged', instance)
