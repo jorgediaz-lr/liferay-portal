d5163fc0ee00cc48afaae8644e0ca3aa503cfbc0
Jorge Díaz <jorge.diaz@liferay.com>
2021-09-24 18:00:27 +0200

    LPS-0 Add DataGuard to some methods

--- modules/apps/depot/depot-test/src/testIntegration/java/com/liferay/depot/service/test/DepotEntryGroupRelLocalServiceTest.java
+++ modules/apps/depot/depot-test/src/testIntegration/java/com/liferay/depot/service/test/DepotEntryGroupRelLocalServiceTest.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
@@ -121,6 +122,7 @@ public class DepotEntryGroupRelLocalServiceTest {
 		}
 	}
 
+	@DataGuard(autoDeleteClassNames = {}, scope = DataGuard.Scope.METHOD)
 	@Test
 	public void testDeleteGroup() throws Exception {
 		DepotEntry depotEntry = _addDepotEntry();
--- modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/service/test/DLFileEntryMetadataLocalServiceTest.java
+++ modules/apps/document-library/document-library-test/src/testIntegration/java/com/liferay/document/library/service/test/DLFileEntryMetadataLocalServiceTest.java
@@ -44,6 +44,7 @@ import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.constants.TestDataConstants;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
@@ -118,6 +119,7 @@ public class DLFileEntryMetadataLocalServiceTest {
 			serviceContext);
 	}
 
+	@DataGuard(autoDeleteClassNames = {}, scope = DataGuard.Scope.METHOD)
 	@Test
 	public void testDeleteGroup() throws Exception {
 		User user = TestPropsValues.getUser();
