b4e933344303cfaff9c8988a696f6c6a5ceef3db
Jorge Díaz <jorge.diaz@liferay.com>
2021-09-28 13:11:09 +0200

    LPS-138761 CompanyLocalServiceTest: Delete the records that are added in the ClassName_ table during the staging and export/import operations as they are not cleaned up after removing the Company and otherwise they will detected as orphan data by DataGuard with autoDelete = false

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
@@ -35,6 +35,7 @@ import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
@@ -47,8 +48,10 @@ import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.model.UserGroupRole;
+import com.liferay.portal.kernel.model.adapter.StagedTheme;
 import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.service.LayoutPrototypeLocalServiceUtil;
@@ -82,12 +85,14 @@ import com.liferay.portal.kernel.util.PrefsPropsUtil;
 import com.liferay.portal.kernel.util.PropsKeys;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.portal.test.rule.SybaseDump;
 import com.liferay.portal.test.rule.SybaseDumpTransactionLog;
 import com.liferay.portal.util.PortalInstances;
 import com.liferay.portal.util.PropsValues;
+import com.liferay.site.model.adapter.StagedGroup;
 import com.liferay.sites.kernel.util.SitesUtil;
 
 import java.io.File;
@@ -262,6 +267,8 @@ public class CompanyLocalServiceTest {
 			companyStagingGroup.getGroupId());
 
 		Assert.assertNull(companyStagingGroup);
+
+		deleteClassName(StagedGroup.class.getName());
 	}
 
 	@Test
@@ -337,6 +344,9 @@ public class CompanyLocalServiceTest {
 				layoutSetPrototype.getLayoutSetPrototypeId());
 
 		Assert.assertNull(layoutSetPrototype);
+
+		deleteClassName(StagedGroup.class.getName());
+		deleteClassName(StagedTheme.class.getName());
 	}
 
 	@Test
@@ -977,6 +987,16 @@ public class CompanyLocalServiceTest {
 			serviceContext);
 	}
 
+	protected void deleteClassName(String value) {
+		ClassName className = _classNameLocalService.fetchClassName(value);
+
+		if (className == null) {
+			return;
+		}
+
+		_classNameLocalService.deleteClassName(className);
+	}
+
 	protected ServiceContext getServiceContext(long companyId) {
 		ServiceContext serviceContext = new ServiceContext();
 
@@ -1145,6 +1165,9 @@ public class CompanyLocalServiceTest {
 
 		_transactionConfig = builder.build();
 	}
+
+	@Inject
+	private ClassNameLocalService _classNameLocalService;
 
 	private long _companyId;
 	private MockServletContext _mockServletContext;
