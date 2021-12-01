2afa951fa61b4ef28fad2ff04f64d74900ce1cf9
Jorge Díaz <jorge.diaz@liferay.com>
2021-12-01 22:23:58 +0100

    LPS-2 Check orphan records after the method

--- portal-test/src/com/liferay/portal/kernel/test/rule/OrphanDetectionTestRule.java
+++ portal-test/src/com/liferay/portal/kernel/test/rule/OrphanDetectionTestRule.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.kernel.test.rule;
 
+import com.liferay.petra.io.unsync.UnsyncPrintWriter;
+import com.liferay.petra.io.unsync.UnsyncStringWriter;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.dao.orm.ORMException;
 import com.liferay.portal.kernel.dao.orm.Session;
@@ -25,6 +27,10 @@ import com.liferay.portal.kernel.model.ShardedModel;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.junit.Assert;
 import org.junit.runner.Description;
 
 import org.osgi.framework.BundleContext;
@@ -34,34 +40,83 @@ import org.osgi.framework.ServiceRegistration;
  * @author Jorge Díaz
  */
 public class OrphanDetectionTestRule
-	extends MethodTestRule<ServiceRegistration<SessionCustomizer>> {
+	extends MethodTestRule<OrphanDetectionTestRule.DataBag> {
 
 	public static final OrphanDetectionTestRule INSTANCE =
 		new OrphanDetectionTestRule();
 
+	public static class DataBag {
+
+		private DataBag(
+			Map<BaseModel<?>, String> records,
+			ServiceRegistration<SessionCustomizer> serviceRegistration) {
+
+			_records = records;
+			_serviceRegistration = serviceRegistration;
+		}
+
+		private final Map<BaseModel<?>, String> _records;
+		private final ServiceRegistration<SessionCustomizer>
+			_serviceRegistration;
+
+	}
+
 	@Override
 	protected void afterMethod(
-			Description description,
-			ServiceRegistration<SessionCustomizer> serviceRegistration,
-			Object target)
+			Description description, DataBag dataBag, Object target)
 		throws Throwable {
+
+		ServiceRegistration<SessionCustomizer> serviceRegistration =
+			dataBag._serviceRegistration;
+
+		Map<BaseModel<?>, String> records = dataBag._records;
+
+		for (Map.Entry<BaseModel<?>, String> entry : records.entrySet()) {
+			_checkOrphanData(entry.getKey(), entry.getValue());
+		}
 
 		serviceRegistration.unregister();
 	}
 
 	@Override
-	protected ServiceRegistration<SessionCustomizer> beforeMethod(
-			Description description, Object target)
+	protected DataBag beforeMethod(Description description, Object target)
 		throws Throwable {
+
+		Map<BaseModel<?>, String> records = new ConcurrentHashMap<>();
 
 		BundleContext bundleContext = SystemBundleUtil.getBundleContext();
 
-		return bundleContext.registerService(
-			SessionCustomizer.class, new OrphanDetectionSessionCustomizer(),
-			null);
+		return new DataBag(
+			records,
+			bundleContext.registerService(
+				SessionCustomizer.class,
+				new OrphanDetectionSessionCustomizer(records), null));
 	}
 
 	private OrphanDetectionTestRule() {
+	}
+
+	private void _checkOrphanData(BaseModel<?> baseModel, String backtraceInfo)
+		throws ORMException {
+
+		if (!(baseModel instanceof ShardedModel)) {
+			return;
+		}
+
+		ShardedModel shardedModel = (ShardedModel)baseModel;
+
+		int count =
+			ResourcePermissionLocalServiceUtil.getResourcePermissionsCount(
+				shardedModel.getCompanyId(), baseModel.getModelClassName(),
+				ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(baseModel.getPrimaryKeyObj()));
+
+		Assert.assertEquals(
+			StringBundler.concat(
+				"Orphan ResourcePermission after the deletion of ",
+				baseModel.getModelClassName(), ": ", baseModel,
+				" with backtraceInfo ", backtraceInfo),
+			0, count);
 	}
 
 	private static class OrphanDetectionSessionCustomizer
@@ -69,8 +124,16 @@ public class OrphanDetectionTestRule
 
 		@Override
 		public Session customize(Session session) {
-			return new OrphanDetectionSessionWrapper(session);
+			return new OrphanDetectionSessionWrapper(session, _records);
 		}
+
+		private OrphanDetectionSessionCustomizer(
+			Map<BaseModel<?>, String> records) {
+
+			_records = records;
+		}
+
+		private Map<BaseModel<?>, String> _records;
 
 	}
 
@@ -80,20 +143,28 @@ public class OrphanDetectionTestRule
 		public void delete(Object object) throws ORMException {
 			super.delete(object);
 
+			if (!(object instanceof BaseModel<?>) ||
+				!_checkOrphanData(object)) {
+
+				return;
+			}
+
+			_record(object);
+		}
+
+		private OrphanDetectionSessionWrapper(
+			Session session, Map<BaseModel<?>, String> records) {
+
+			super(session);
+
+			_records = records;
+		}
+
+		private boolean _checkOrphanData(Object object) {
 			BaseModel<?> baseModel = (BaseModel<?>)object;
 
-			_checkOrphanData(baseModel);
-		}
-
-		private OrphanDetectionSessionWrapper(Session session) {
-			super(session);
-		}
-
-		private void _checkOrphanData(BaseModel<?> baseModel)
-			throws ORMException {
-
 			if (!(baseModel instanceof ShardedModel)) {
-				return;
+				return false;
 			}
 
 			ShardedModel shardedModel = (ShardedModel)baseModel;
@@ -104,13 +175,41 @@ public class OrphanDetectionTestRule
 					ResourceConstants.SCOPE_INDIVIDUAL,
 					String.valueOf(baseModel.getPrimaryKeyObj()));
 
-			if (count > 0) {
-				throw new ORMException(
-					StringBundler.concat(
-						"Orphan ResourcePermission after the deletion of ",
-						baseModel.getModelClassName(), ": ", baseModel));
+			if (count == 0) {
+				return false;
 			}
+
+			return true;
 		}
+
+		private void _record(Object object) {
+			BaseModel<?> baseModel = (BaseModel<?>)object;
+
+			if (baseModel.isNew()) {
+				return;
+			}
+
+			Thread currentThread = Thread.currentThread();
+
+			UnsyncStringWriter unsyncStringWriter = new UnsyncStringWriter();
+
+			unsyncStringWriter.write("Thread name : ");
+			unsyncStringWriter.write(currentThread.getName());
+			unsyncStringWriter.write(", id : ");
+			unsyncStringWriter.write(String.valueOf(currentThread.getId()));
+			unsyncStringWriter.write(", created : ");
+			unsyncStringWriter.write(baseModel.toString());
+			unsyncStringWriter.write(" at \n");
+
+			Exception exception = new Exception();
+
+			exception.printStackTrace(
+				new UnsyncPrintWriter(unsyncStringWriter));
+
+			_records.put(baseModel, unsyncStringWriter.toString());
+		}
+
+		private Map<BaseModel<?>, String> _records;
 
 	}
 
