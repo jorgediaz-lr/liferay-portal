c5fc9030ad34d23fb5be539d6c7deda467b346db
Jorge Díaz <jorge.diaz@liferay.com>
2022-05-07 16:31:16 +0200

    Revert "LPS-145396 Simplify individual scope deletion"
    This reverts commit 8e92215fd1a1665a79b9394b49a7339549135d18.

--- portal-test/src/com/liferay/portal/kernel/test/util/ResourcePermissionTestUtil.java
+++ portal-test/src/com/liferay/portal/kernel/test/util/ResourcePermissionTestUtil.java
@@ -18,17 +18,23 @@ import com.liferay.counter.kernel.service.CounterLocalServiceUtil;
 import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
 import com.liferay.petra.sql.dsl.query.DSLQuery;
 import com.liferay.portal.kernel.model.BaseModel;
+import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.PersistedModel;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.ResourcePermission;
 import com.liferay.portal.kernel.model.ResourcePermissionTable;
+import com.liferay.portal.kernel.model.ResourcedModel;
 import com.liferay.portal.kernel.model.ShardedModel;
+import com.liferay.portal.kernel.model.TypedModel;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
+import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
 import com.liferay.portal.kernel.service.ResourcePermissionLocalServiceUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Alberto Chaparro
@@ -117,9 +123,54 @@ public class ResourcePermissionTestUtil {
 
 		ShardedModel shardedModel = (ShardedModel)baseModel;
 
-		_deleteResourcePermissions(
-			shardedModel.getCompanyId(), ResourceConstants.SCOPE_INDIVIDUAL,
+		ResourcePermissionLocalServiceUtil.deleteResourcePermissions(
+			shardedModel.getCompanyId(), baseModel.getModelClassName(),
+			ResourceConstants.SCOPE_INDIVIDUAL,
 			String.valueOf(baseModel.getPrimaryKeyObj()));
+
+		if (persistedModel instanceof TypedModel) {
+			TypedModel typedModel = (TypedModel)persistedModel;
+
+			ClassName typedModelClassName =
+				ClassNameLocalServiceUtil.fetchByClassNameId(
+					typedModel.getClassNameId());
+
+			if (typedModelClassName != null) {
+				Map<String, Object> modelAttributes =
+					baseModel.getModelAttributes();
+
+				if (modelAttributes.containsKey("resourceClassNameId")) {
+					ClassName resourceClassName =
+						ClassNameLocalServiceUtil.fetchByClassNameId(
+							(Long)modelAttributes.get("resourceClassNameId"));
+
+					if (resourceClassName != null) {
+						typedModelClassName = resourceClassName;
+					}
+				}
+
+				String compositeModelName =
+					ResourceActionsUtil.getCompositeModelName(
+						typedModelClassName.getValue(),
+						baseModel.getModelClassName());
+
+				ResourcePermissionLocalServiceUtil.deleteResourcePermissions(
+					shardedModel.getCompanyId(), compositeModelName,
+					ResourceConstants.SCOPE_INDIVIDUAL,
+					String.valueOf(baseModel.getPrimaryKeyObj()));
+			}
+		}
+
+		if (!(persistedModel instanceof ResourcedModel)) {
+			return;
+		}
+
+		ResourcedModel resourcedModel = (ResourcedModel)baseModel;
+
+		ResourcePermissionLocalServiceUtil.deleteResourcePermissions(
+			shardedModel.getCompanyId(), baseModel.getModelClassName(),
+			ResourceConstants.SCOPE_INDIVIDUAL,
+			String.valueOf(resourcedModel.getResourcePrimKey()));
 	}
 
 	private static void _deleteResourcePermissions(
