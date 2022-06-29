6dcb30425fc2b7da89ad3133d808a17fc9b45df3
Jorge Díaz <jorge.diaz@liferay.com>
2022-06-29 15:07:03 +0200

    LPS-145396 The resourceName of DDMStructures are composed, so we have to check them. The AccountEntry and AccountGroup records can have an additional ResourcePermission with the CommerceAccount and CommerceAccountGroup name value

--- portal-test/src/com/liferay/portal/kernel/test/util/ResourcePermissionTestUtil.java
+++ portal-test/src/com/liferay/portal/kernel/test/util/ResourcePermissionTestUtil.java
@@ -118,10 +118,29 @@ public class ResourcePermissionTestUtil {
 
 		ShardedModel shardedModel = (ShardedModel)baseModel;
 
+		String resourceName = baseModel.getModelClassName();
+
+		if (resourceName.contains(_DDM_STRUCTURE)) {
+			resourceName = _DDM_STRUCTURE;
+		}
+
 		ResourcePermissionLocalServiceUtil.deleteResourcePermissions(
-			shardedModel.getCompanyId(), baseModel.getModelClassName(),
+			shardedModel.getCompanyId(), resourceName,
 			ResourceConstants.SCOPE_INDIVIDUAL,
 			String.valueOf(baseModel.getPrimaryKeyObj()));
+
+		if (resourceName.equals(_ACCOUNT_ENTRY)) {
+			ResourcePermissionLocalServiceUtil.deleteResourcePermissions(
+				shardedModel.getCompanyId(), _COMMERCE_ACCOUNT,
+				ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(baseModel.getPrimaryKeyObj()));
+		}
+		else if (resourceName.equals(_ACCOUNT_GROUP)) {
+			ResourcePermissionLocalServiceUtil.deleteResourcePermissions(
+				shardedModel.getCompanyId(), _COMMERCE_ACCOUNT_GROUP,
+				ResourceConstants.SCOPE_INDIVIDUAL,
+				String.valueOf(baseModel.getPrimaryKeyObj()));
+		}
 
 		if (!(persistedModel instanceof ResourcedModel)) {
 			return;
@@ -130,7 +149,7 @@ public class ResourcePermissionTestUtil {
 		ResourcedModel resourcedModel = (ResourcedModel)baseModel;
 
 		ResourcePermissionLocalServiceUtil.deleteResourcePermissions(
-			shardedModel.getCompanyId(), baseModel.getModelClassName(),
+			shardedModel.getCompanyId(), resourceName,
 			ResourceConstants.SCOPE_INDIVIDUAL,
 			String.valueOf(resourcedModel.getResourcePrimKey()));
 	}
@@ -161,5 +180,20 @@ public class ResourcePermissionTestUtil {
 				resourcePermissionId);
 		}
 	}
+
+	private static final String _ACCOUNT_ENTRY =
+		"com.liferay.account.model.AccountEntry";
+
+	private static final String _ACCOUNT_GROUP =
+		"com.liferay.account.model.AccountGroup";
+
+	private static final String _COMMERCE_ACCOUNT =
+		"com.liferay.commerce.account.model.CommerceAccount";
+
+	private static final String _COMMERCE_ACCOUNT_GROUP =
+		"com.liferay.commerce.account.model.CommerceAccountGroup";
+
+	private static final String _DDM_STRUCTURE =
+		"com.liferay.dynamic.data.mapping.model.DDMStructure";
 
 }
