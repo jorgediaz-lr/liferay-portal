d8c2c5a5d612b2324518bcd33520c6d2251f57d4
Jorge Díaz <jorge.diaz@liferay.com>
2022-08-23 14:33:31 +0200

    LPS-145396 [create subtask] Add deleteDepotEntry(DepotEntry) method

--- modules/apps/depot/depot-service/src/main/java/com/liferay/depot/service/impl/DepotEntryLocalServiceImpl.java
+++ modules/apps/depot/depot-service/src/main/java/com/liferay/depot/service/impl/DepotEntryLocalServiceImpl.java
@@ -155,21 +155,29 @@ public class DepotEntryLocalServiceImpl extends DepotEntryLocalServiceBaseImpl {
 	}
 
 	@Override
+	public DepotEntry deleteDepotEntry(DepotEntry depotEntry)
+		throws PortalException {
+
+		if (_isStaged(depotEntry)) {
+			throw new DepotEntryStagedException(
+				"Unstage depot entry " + depotEntry.getDepotEntryId() +
+					" before deleting it");
+		}
+
+		_resourceLocalService.deleteResource(
+			depotEntry, ResourceConstants.SCOPE_INDIVIDUAL);
+
+		return super.deleteDepotEntry(depotEntry);
+	}
+
+	@Override
 	public DepotEntry deleteDepotEntry(long depotEntryId)
 		throws PortalException {
 
 		DepotEntry depotEntry = depotEntryPersistence.fetchByPrimaryKey(
 			depotEntryId);
 
-		if (_isStaged(depotEntry)) {
-			throw new DepotEntryStagedException(
-				"Unstage depot entry " + depotEntryId + " before deleting it");
-		}
-
-		_resourceLocalService.deleteResource(
-			depotEntry, ResourceConstants.SCOPE_INDIVIDUAL);
-
-		return super.deleteDepotEntry(depotEntryId);
+		return deleteDepotEntry(depotEntry);
 	}
 
 	@Override
