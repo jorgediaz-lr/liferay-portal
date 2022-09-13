86a6fd45135bc10b5c8c1a5fa9a534a12c34c569
Jorge Díaz <jorge.diaz@liferay.com>
2022-09-13 12:57:25 +0200

    LPS-1 Remove article.getContent() usage in JournalEditArticleDisplayContext

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalEditArticleDisplayContext.java
@@ -57,7 +57,6 @@ import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -413,8 +412,14 @@ public class JournalEditArticleDisplayContext {
 				siteDefaultLocale);
 		}
 		else {
-			_defaultArticleLanguageId = LocalizationUtil.getDefaultLanguageId(
-				_article.getContent(), siteDefaultLocale);
+			DDMStructure ddmStructure = _article.getDDMStructure();
+
+			DDMFormValues ddmFormValues =
+				DDMFieldLocalServiceUtil.getDDMFormValues(
+					ddmStructure.getDDMForm(), _article.getId());
+
+			_defaultArticleLanguageId = LocaleUtil.toLanguageId(
+				ddmFormValues.getDefaultLocale());
 		}
 
 		return _defaultArticleLanguageId;
