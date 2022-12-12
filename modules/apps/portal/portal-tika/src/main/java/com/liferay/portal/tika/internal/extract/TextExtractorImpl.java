560f0b87d543cd7a355fb4aae645720d88e632ad
Jorge Díaz <jorge.diaz@liferay.com>
2022-12-12 20:22:17 +0100

    LPS-169760 Use getTikaConfigInputStream() to avoid having problems reading the configuration when executing a child process

--- modules/apps/portal/portal-tika/src/main/java/com/liferay/portal/tika/internal/extract/TextExtractorImpl.java
+++ modules/apps/portal/portal-tika/src/main/java/com/liferay/portal/tika/internal/extract/TextExtractorImpl.java
@@ -41,6 +41,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.apache.tika.Tika;
+import org.apache.tika.config.TikaConfig;
 import org.apache.tika.exception.TikaException;
 import org.apache.tika.extractor.EmbeddedDocumentExtractor;
 import org.apache.tika.extractor.ParsingEmbeddedDocumentExtractor;
@@ -100,7 +101,9 @@ public class TextExtractorImpl implements TextExtractor {
 					_processExecutor.execute(
 						ProcessConfigUtil.getProcessConfig(),
 						new ExtractTextProcessCallable(
-							StreamUtil.toByteArray(finalInputStream)));
+							StreamUtil.toByteArray(finalInputStream),
+							StreamUtil.toString(
+								_tikaConfigHelper.getTikaConfigInputStream())));
 
 				Future<String> future =
 					processChannel.getProcessNoticeableFuture();
@@ -230,9 +233,12 @@ public class TextExtractorImpl implements TextExtractor {
 
 			logger.setLevel(Level.SEVERE);
 
-			Tika tika = new Tika(_tikaConfigHelper.getTikaConfig());
-
 			try {
+				Tika tika = new Tika(
+					new TikaConfig(
+						new UnsyncByteArrayInputStream(
+							_tikaConfig.getBytes())));
+
 				return _parseToString(
 					tika, new UnsyncByteArrayInputStream(_data));
 			}
@@ -241,13 +247,15 @@ public class TextExtractorImpl implements TextExtractor {
 			}
 		}
 
-		private ExtractTextProcessCallable(byte[] data) {
+		private ExtractTextProcessCallable(byte[] data, String tikaConfig) {
 			_data = data;
+			_tikaConfig = tikaConfig;
 		}
 
 		private static final long serialVersionUID = 1L;
 
 		private final byte[] _data;
+		private final String _tikaConfig;
 
 	}
 
