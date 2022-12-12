fb75ed01829c0fce45ca63659047753c4e0f1418
Jorge Díaz <jorge.diaz@liferay.com>
2022-12-12 16:36:19 +0100

    LPS-169760 New PortalTikaConfiguration system configuration for the portal tika module

--- modules/apps/portal/portal-tika/src/main/java/com/liferay/portal/tika/internal/util/TikaConfigUtil.java
+++ modules/apps/portal/portal-tika/src/main/java/com/liferay/portal/tika/internal/util/TikaConfigUtil.java
@@ -14,27 +14,70 @@
 
 package com.liferay.portal.tika.internal.util;
 
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.tika.internal.configuration.PortalTikaConfiguration;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.Map;
+
 import org.apache.tika.config.TikaConfig;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 
 /**
  * @author Shuyang Zhou
+ * @author Jorge Díaz
  */
+@Component(
+	configurationPid = "com.liferay.portal.tika.internal.configuration.PortalTikaConfiguration",
+	service = {}
+)
 public class TikaConfigUtil {
 
 	public static TikaConfig getTikaConfig() {
 		return _tikaConfig;
 	}
 
-	private static final TikaConfig _tikaConfig;
+	public static InputStream getTikaConfigInputStream()
+		throws FileNotFoundException {
 
-	static {
+		String tikaConfigPath = _portalTikaConfiguration.tikaConfigPath();
+
+		if (Validator.isNotNull(tikaConfigPath)) {
+			Path path = Paths.get(tikaConfigPath);
+
+			return new FileInputStream(path.toFile());
+		}
+
+		return TikaConfigUtil.class.getResourceAsStream(
+			"dependencies/tika.xml");
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_portalTikaConfiguration = ConfigurableUtil.createConfigurable(
+			PortalTikaConfiguration.class, properties);
+
 		try {
-			_tikaConfig = new TikaConfig(
-				TikaConfigUtil.class.getResource("dependencies/tika.xml"));
+			_tikaConfig = new TikaConfig(getTikaConfigInputStream());
 		}
 		catch (Exception exception) {
-			throw new ExceptionInInitializerError(exception);
+			throw new SystemException(exception);
 		}
 	}
+
+	private static volatile PortalTikaConfiguration _portalTikaConfiguration;
+	private static volatile TikaConfig _tikaConfig;
 
 }
