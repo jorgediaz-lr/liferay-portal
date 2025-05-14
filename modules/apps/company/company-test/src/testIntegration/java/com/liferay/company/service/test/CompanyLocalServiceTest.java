2fcc3546cd4b71c2519c0e60070aae50d7c7834a
Jorge Díaz <jorge.diaz@liferay.com>
2025-05-14 16:17:39 +0200

    LPD-53172 Avoid errors in DataGuardTestRuleUtil caused by com.liferay.commerce.product.content.web.internal.portlet.CPCompareContentMiniPortlet

--- modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
+++ modules/apps/company/company-test/src/testIntegration/java/com/liferay/company/service/test/CompanyLocalServiceTest.java
@@ -235,6 +235,7 @@ public class CompanyLocalServiceTest {
 		deleteClassName(_layoutFriendlyURLEntryHelper.getClassName(true));
 		deleteClassName("com.liferay.commerce.order.content.web.internal.portlet.CommerceOrderContentPortlet");
 		deleteClassName("com.liferay.commerce.product.content.web.internal.portlet.CPContentPortlet");
+		deleteClassName("com.liferay.commerce.product.content.web.internal.portlet.CPCompareContentMiniPortlet");
 		deleteClassName("com.liferay.info.item.provider.InfoItemFormProvider");
 		deleteStagingClassNameEntries();
 	}
