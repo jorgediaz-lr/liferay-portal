7b2247052b3f6860158b8441be426f49bb105935
Jorge Díaz <jorge.diaz@liferay.com>
2025-09-03 13:49:41 +0200

    LPD-62088 Extract the PortalInstances.getCompanyId from AbsoluteRedirectsFilter to the ValidHostNameFilter (first filter with /* url-pattern). We also add this filter to the ERROR dispatcher because we want to set the CompanyThreadLocal to the error pages requests to avoid database partition problems (see related LPD-60239).

--- portal-impl/src/com/liferay/portal/servlet/filters/validhostname/ValidHostNameFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/validhostname/ValidHostNameFilter.java
@@ -5,14 +5,29 @@
 
 package com.liferay.portal.servlet.filters.validhostname;
 
+import com.liferay.portal.kernel.exception.NoSuchVirtualHostException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.servlet.TryFilter;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.PropsValues;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.servlet.filters.BasePortalFilter;
+import com.liferay.portal.util.PortalInstances;
 
+import jakarta.servlet.DispatcherType;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 /**
+ * <p>
+ * This filter checks the host name is valid. It should not be disabled because
+ * it also sets the company ID in the request so that subsequent calls in the
+ * thread have the company ID properly set. This filter should also always be
+ * the first filter in the list of filters.
+ * </p>
+ *
  * @author Shuyang Zhou
  */
 public class ValidHostNameFilter extends BasePortalFilter implements TryFilter {
@@ -22,6 +37,25 @@ public class ValidHostNameFilter extends BasePortalFilter implements TryFilter {
 			HttpServletRequest httpServletRequest,
 			HttpServletResponse httpServletResponse)
 		throws Exception {
+
+		// Company ID needs to always be called here so that it is properly set
+		// in subsequent calls
+
+		try {
+			PortalInstances.getCompanyId(
+				httpServletRequest,
+				_isVirtualHostsStrictAccess(httpServletRequest));
+		}
+		catch (NoSuchVirtualHostException noSuchVirtualHostException) {
+			_log.error(noSuchVirtualHostException);
+
+			httpServletRequest.setAttribute(
+				WebKeys.UNKNOWN_VIRTUAL_HOST, Boolean.TRUE);
+
+			httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
+
+			return null;
+		}
 
 		String serverName = httpServletRequest.getServerName();
 
@@ -33,5 +67,30 @@ public class ValidHostNameFilter extends BasePortalFilter implements TryFilter {
 
 		return null;
 	}
+
+	@Override
+	public boolean isFilterEnabled() {
+		return _FILTER_ENABLED;
+	}
+
+	private boolean _isVirtualHostsStrictAccess(
+		HttpServletRequest httpServletRequest) {
+
+		if (!PropsValues.VIRTUAL_HOSTS_STRICT_ACCESS ||
+			((httpServletRequest.getDispatcherType() == DispatcherType.ERROR) &&
+			 GetterUtil.getBoolean(
+				 httpServletRequest.getAttribute(
+					 WebKeys.UNKNOWN_VIRTUAL_HOST)))) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+	private static final boolean _FILTER_ENABLED = true;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ValidHostNameFilter.class);
 
 }
