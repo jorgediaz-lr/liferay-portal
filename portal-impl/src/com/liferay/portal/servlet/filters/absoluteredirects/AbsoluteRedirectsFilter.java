8b218451f170569b848f4f1d542dc5f7b48e09cd
Jorge Díaz <jorge.diaz@liferay.com>
2025-09-03 13:49:41 +0200

    LPD-62088 Extract the PortalInstances.getCompanyId from AbsoluteRedirectsFilter to the ValidHostNameFilter (first filter with /* url-pattern).

--- portal-impl/src/com/liferay/portal/servlet/filters/absoluteredirects/AbsoluteRedirectsFilter.java
+++ portal-impl/src/com/liferay/portal/servlet/filters/absoluteredirects/AbsoluteRedirectsFilter.java
@@ -6,7 +6,6 @@
 package com.liferay.portal.servlet.filters.absoluteredirects;
 
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.exception.NoSuchVirtualHostException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -15,10 +14,8 @@ import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.servlet.TryFilter;
 import com.liferay.portal.kernel.servlet.WrapHttpServletResponseFilter;
 import com.liferay.portal.kernel.util.PortalUtil;
-import com.liferay.portal.kernel.util.PropsValues;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.servlet.filters.BasePortalFilter;
-import com.liferay.portal.util.PortalInstances;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -27,9 +24,7 @@ import jakarta.servlet.http.HttpSession;
 /**
  * <p>
  * This filter is used to ensure that all redirects are absolute. It should not
- * be disabled because it also sets the company ID in the request so that
- * subsequent calls in the thread have the company ID properly set. This filter
- * should also always be the first filter in the list of filters.
+ * be disabled.
  * </p>
  *
  * @author Minhchau Dang
@@ -50,24 +45,6 @@ public class AbsoluteRedirectsFilter
 		}
 
 		//response.setContentType(ContentTypes.TEXT_HTML_UTF8);
-
-		// Company ID needs to always be called here so that it is properly set
-		// in subsequent calls
-
-		try {
-			PortalInstances.getCompanyId(
-				httpServletRequest, PropsValues.VIRTUAL_HOSTS_STRICT_ACCESS);
-		}
-		catch (NoSuchVirtualHostException noSuchVirtualHostException) {
-			_log.error(noSuchVirtualHostException);
-
-			httpServletRequest.setAttribute(
-				WebKeys.UNKNOWN_VIRTUAL_HOST, Boolean.TRUE);
-
-			httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
-
-			return null;
-		}
 
 		PortalUtil.getCurrentCompleteURL(httpServletRequest);
 		PortalUtil.getCurrentURL(httpServletRequest);
