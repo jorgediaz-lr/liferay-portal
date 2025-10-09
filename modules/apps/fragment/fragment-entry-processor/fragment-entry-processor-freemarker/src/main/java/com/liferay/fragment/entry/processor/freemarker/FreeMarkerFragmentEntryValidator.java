11c3f76083f5f713447759a37f122ea5cef4b81b
Jorge Díaz <jorge.diaz@liferay.com>
2025-10-09 13:56:34 +0200

    LPP-60743 Debug traces in FreeMarkerFragmentEntryValidator.validateFragmentEntryHTML

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-freemarker/src/main/java/com/liferay/fragment/entry/processor/freemarker/FreeMarkerFragmentEntryValidator.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-freemarker/src/main/java/com/liferay/fragment/entry/processor/freemarker/FreeMarkerFragmentEntryValidator.java
@@ -5,17 +5,21 @@
 
 package com.liferay.fragment.entry.processor.freemarker;
 
+import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
 import com.liferay.fragment.entry.processor.freemarker.internal.configuration.FreeMarkerFragmentEntryProcessorConfiguration;
 import com.liferay.fragment.exception.FragmentEntryContentException;
 import com.liferay.fragment.input.template.parser.InputTemplateNode;
 import com.liferay.fragment.processor.FragmentEntryValidator;
 import com.liferay.fragment.util.configuration.FragmentEntryConfigurationParser;
 import com.liferay.petra.io.DummyWriter;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
@@ -26,6 +30,7 @@ import com.liferay.portal.kernel.template.Template;
 import com.liferay.portal.kernel.template.TemplateConstants;
 import com.liferay.portal.kernel.template.TemplateException;
 import com.liferay.portal.kernel.template.TemplateManagerUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.Validator;
@@ -33,8 +38,12 @@ import com.liferay.portal.kernel.util.WebKeys;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.text.SimpleDateFormat;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
@@ -76,6 +85,94 @@ public class FreeMarkerFragmentEntryValidator
 		if (serviceContext != null) {
 			httpServletRequest = serviceContext.getRequest();
 			httpServletResponse = serviceContext.getResponse();
+		}
+
+		if (_log.isDebugEnabled()) {
+			_log.debug("=== Fragment Entry Validator Debug Info ===");
+			_log.debug("httpServletRequest: " + httpServletRequest);
+
+			if (httpServletRequest != null) {
+				String remoteUser = httpServletRequest.getRemoteUser();
+				String timestamp = new SimpleDateFormat(
+					"dd/MMM/yyyy:HH:mm:ss Z"
+				).format(
+					new Date()
+				);
+				String requestLine = StringBundler.concat(
+					httpServletRequest.getMethod(), " ",
+					httpServletRequest.getRequestURI(),
+					(httpServletRequest.getQueryString() != null) ?
+						"?" + httpServletRequest.getQueryString() : "",
+					" ", httpServletRequest.getProtocol());
+				String referer = httpServletRequest.getHeader("Referer");
+				String userAgent = httpServletRequest.getHeader("User-Agent");
+
+				_log.debug(
+					String.format(
+						"httpServletRequest: %s - %s [%s] \"%s\" \"%s\" \"%s\"",
+						httpServletRequest.getRemoteAddr(),
+						(remoteUser != null) ? remoteUser : "-", timestamp,
+						requestLine, (referer != null) ? referer : "-",
+						(userAgent != null) ? userAgent : "-"));
+
+				ThemeDisplay themeDisplay =
+					(ThemeDisplay)httpServletRequest.getAttribute(
+						WebKeys.THEME_DISPLAY);
+
+				_log.debug(
+					"httpServletRequest.getAttribute(THEME_DISPLAY): " +
+						themeDisplay);
+
+				if (themeDisplay != null) {
+					_log.debug(
+						String.format(
+							"ThemeDisplay - Company: %d, Group: %d, User: %s (ID: %d), Layout: %d, SignedIn: %b, Locale: %s, TimeZone: %s",
+							themeDisplay.getCompanyId(),
+							themeDisplay.getScopeGroupId(),
+							themeDisplay.getUser(
+							).getFullName(),
+							themeDisplay.getUserId(), themeDisplay.getPlid(),
+							themeDisplay.isSignedIn(), themeDisplay.getLocale(),
+							themeDisplay.getTimeZone(
+							).getID()));
+					_log.debug(
+						StringBundler.concat(
+							"Portal URL: ", themeDisplay.getPortalURL(),
+							", Current URL: ", themeDisplay.getURLCurrent()));
+				}
+
+				HttpSession httpSession = httpServletRequest.getSession(false);
+
+				_log.debug(
+					"Session: " +
+						((httpSession != null) ? httpSession.getId() : "null"));
+			}
+
+			_log.debug("serviceContext: " + serviceContext);
+
+			if (serviceContext != null) {
+				_log.debug(
+					String.format(
+						"ServiceContext - Company: %d, Group: %d, User: %d, Command: %s, UUID: %s",
+						serviceContext.getCompanyId(),
+						serviceContext.getScopeGroupId(),
+						serviceContext.getUserId(), serviceContext.getCommand(),
+						serviceContext.getUuid()));
+				_log.debug(
+					"ServiceContext Current URL: " +
+						serviceContext.getCurrentURL());
+			}
+
+			_log.debug(
+				String.format(
+					"Thread: %s, Export: %b, Import: %b, Staging: %b",
+					Thread.currentThread(
+					).getName(),
+					ExportImportThreadLocal.isExportInProcess(),
+					ExportImportThreadLocal.isImportInProcess(),
+					ExportImportThreadLocal.isStagingInProcess()));
+
+			_log.debug("===========================================");
 		}
 
 		if ((httpServletRequest == null) ||
@@ -167,6 +264,8 @@ public class FreeMarkerFragmentEntryValidator
 
 		return false;
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog("LPP-60743");
 
 	@Reference
 	private ConfigurationProvider _configurationProvider;
