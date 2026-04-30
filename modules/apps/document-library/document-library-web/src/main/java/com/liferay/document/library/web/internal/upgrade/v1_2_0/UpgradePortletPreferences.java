89634551dab786a6fba6a79641a612ab29744029
Jorge Díaz <jorge.diaz@liferay.com>
2026-04-30 13:30:28 +0200

    LPD-72632 [TEMP] Catch all exceptions in UpgradePortletPreferences.upgradeRootFolderId as workaround for database upgrade

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/upgrade/v1_2_0/UpgradePortletPreferences.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/upgrade/v1_2_0/UpgradePortletPreferences.java
@@ -10,7 +10,6 @@ import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLAppLocalService;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
@@ -73,12 +72,12 @@ public class UpgradePortletPreferences
 				rootFolderExternalReferenceCode =
 					folder.getExternalReferenceCode();
 			}
-			catch (PortalException portalException) {
+			catch (Exception exception) {
 				if (_log.isWarnEnabled()) {
 					_log.warn(
 						StringBundler.concat(
 							"Unable to get root folder ID ", rootFolderId, ": ",
-							portalException));
+							exception));
 				}
 
 				return _toXML(portletPreferences);
