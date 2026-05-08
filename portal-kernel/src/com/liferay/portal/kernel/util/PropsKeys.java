cebbdc33fc14f6af0b7022a5a1aed666f0630135
Jorge Avalos <jorge.avalos@liferay.com>
2026-05-07 18:06:47 -0700

    LPD-84027 Add UpgradeQueryMonitor to log locked queries during upgrades

--- portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/PropsKeys.java
@@ -2730,6 +2730,9 @@ public interface PropsKeys {
 	public static final String UPGRADE_LOG_PROGRESS_INTERVAL =
 		"upgrade.log.progress.interval";
 
+	public static final String UPGRADE_QUERY_MONITOR_ENABLED =
+		"upgrade.query.monitor.enabled";
+
 	public static final String UPGRADE_QUERY_MONITOR_LOCK_THRESHOLD =
 		"upgrade.query.monitor.lock.threshold";
 
