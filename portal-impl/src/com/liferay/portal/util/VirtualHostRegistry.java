5e5f7177c7e6dc8e17cb1c8a792dcfb3a529a33d
IstvanD <istvan.dezsi@liferay.com>
2026-07-02 16:52:05 +0200

    LPD-97113 Add in memory Company and VirtualHost registries

---
+++ portal-impl/src/com/liferay/portal/util/VirtualHostRegistry.java
@@ -0,0 +1,57 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.util;
+
+import com.liferay.portal.kernel.model.VirtualHost;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author István András Dézsi
+ */
+public class VirtualHostRegistry {
+
+	public static VirtualHost fetchVirtualHost(String hostname) {
+		if (Validator.isNull(hostname)) {
+			return null;
+		}
+
+		return _virtualHosts.get(StringUtil.toLowerCase(hostname));
+	}
+
+	public static void register(VirtualHost virtualHost) {
+		String hostname = virtualHost.getHostname();
+
+		if (Validator.isNull(hostname)) {
+			return;
+		}
+
+		_virtualHosts.put(StringUtil.toLowerCase(hostname), virtualHost);
+	}
+
+	public static void unregister(String hostname) {
+		if (Validator.isNull(hostname)) {
+			return;
+		}
+
+		_virtualHosts.remove(StringUtil.toLowerCase(hostname));
+	}
+
+	public static void unregisterVirtualHosts(long companyId) {
+		Collection<VirtualHost> virtualHosts = _virtualHosts.values();
+
+		virtualHosts.removeIf(
+			virtualHost -> virtualHost.getCompanyId() == companyId);
+	}
+
+	private static final Map<String, VirtualHost> _virtualHosts =
+		new ConcurrentHashMap<>();
+
+}
