e2f0aa68a4b80444c4438b0749f3c853c1a1e012
IstvanD <istvan.dezsi@liferay.com>
2026-07-24 14:40:53 +0200

    LPD-96720 Sync default owned Company columns to secondary partitions

--- portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/CompanyLocalServiceImpl.java
@@ -1201,7 +1201,9 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		company.setMaxUsers(maxUsers);
 		company.setActive(active);
 
-		companyPersistence.update(company);
+		company = companyPersistence.update(company);
+
+		_syncDBPartitionCompany(company);
 
 		// Virtual host
 
@@ -1276,7 +1278,9 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 
 		CompanyInfo companyInfo = company.getCompanyInfo();
 
-		companyPersistence.update(company);
+		company = companyPersistence.update(company);
+
+		_syncDBPartitionCompany(company);
 
 		company = updateVirtualHostname(companyId, virtualHostname);
 
@@ -2560,6 +2564,35 @@ public class CompanyLocalServiceImpl extends CompanyLocalServiceBaseImpl {
 		}
 		catch (Throwable throwable) {
 			throw new PortalException(throwable);
+		}
+	}
+
+	private void _syncDBPartitionCompany(Company company) {
+		if (!PropsValues.DATABASE_PARTITION_ENABLED) {
+			return;
+		}
+
+		long companyId = company.getCompanyId();
+
+		if (companyId == PortalInstancePool.getDefaultCompanyId()) {
+			return;
+		}
+
+		try (SafeCloseable safeCloseable =
+				CompanyThreadLocal.setCompanyIdWithSafeCloseable(companyId)) {
+
+			Company partitionCompany = companyPersistence.fetchByPrimaryKey(
+				companyId);
+
+			if (partitionCompany == null) {
+				return;
+			}
+
+			partitionCompany.setMx(company.getMx());
+			partitionCompany.setMaxUsers(company.getMaxUsers());
+			partitionCompany.setActive(company.isActive());
+
+			companyPersistence.update(partitionCompany);
 		}
 	}
 
